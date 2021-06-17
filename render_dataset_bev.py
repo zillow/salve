5cec5299a493fa36e5403427940c001786701fbd
John Lambert <johnlam@zillowgroup.com>
2021-06-17 17:26:28 -0400

    add some more semantic rendering code

--- render_dataset_bev.py
+++ render_dataset_bev.py
@@ -7,7 +7,7 @@ from types import SimpleNamespace
 import imageio
 
 from infer_depth import infer_depth
-from vis_depth import vis_depth
+from vis_depth import vis_depth, vis_depth_and_render
 
 
 def render_dataset():
@@ -25,23 +25,34 @@ def render_dataset():
             "inp": img_fpath,
             "opts": []
         })
+        #infer_depth(args)
 
-        infer_depth(args)
+        is_semantics = False
+        semantic_img_fpath = f"/Users/johnlam/Downloads/MSeg_output/{Path(img_fpath).stem}_gray.jpg"
 
-
+        if is_semantics:
+            crop_z_above = 2.0
+        else:
+            crop_z_above = -1.0
 
         args = SimpleNamespace(**{
-            "img": img_fpath,
+            "img": semantic_img_fpath if is_semantics else img_fpath,
             "depth": f"assets/{Path(img_fpath).stem}.depth.png",
             "scale": 0.001,
-            "crop_ratio": 80/512,
-            "crop_z_above": 0.3 # 1.2
+            "crop_ratio": 80/512, # throw away top 80 and bottom 80 rows of pixel (too noisy of estimates)
+            "crop_z_above": crop_z_above #0.3 # -1.0 # -0.5 # 0.3 # 1.2
         })
-        bev_img = vis_depth(args)
+        bev_img = vis_depth_and_render(args, is_semantics=False)
 
-        save_dir = f"/Users/johnlam/Downloads/ZinD_BEV/{building_id}"
+        #vis_depth(args)
+
+        save_dir = f"/Users/johnlam/Downloads/ZinD_BEV_crop_above_{args.crop_z_above}/{building_id}"
         os.makedirs(save_dir, exist_ok=True)
-        imageio.imwrite(f"{save_dir}/{Path(img_fpath).stem}.jpg", bev_img)
+        if is_semantics:
+            img_name = f"semantics_{Path(img_fpath).stem}.jpg"
+        else:
+            img_name = f"{Path(img_fpath).stem}.jpg"
+        imageio.imwrite(f"{save_dir}/{img_name}", bev_img)
 
 
 if __name__ == "__main__":
