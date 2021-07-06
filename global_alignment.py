4703b4e25704f40abe7d7f86aece747ce8fa7519
John Lambert <johnlam@zillowgroup.com>
2021-07-06 11:20:32 -0400

    check in mST and shonan code

--- global_alignment.py
+++ global_alignment.py
@@ -5,34 +5,62 @@
 Global alignment.
 """
 
+import copy
 import glob
+from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
-from typing import NamedTuple
+from typing import Dict, List, NamedTuple, Optional, Tuple
 
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
 from argoverse.utils.json_utils import read_json_file
+from argoverse.utils.sim2 import Sim2
+from gtsam import Rot3
 
-from cycle_consistency import filter_to_cycle_consistent_edges
+import cycle_consistency as cycle_utils
+
+import gtsfm.utils.graph as graph_utils
+import gtsfm.utils.logger as logger_utils
+
+logger = logger_utils.get_logger()
 
 
-class TwoViewEdge(NamedTuple)
+# class TwoViewEdge(NamedTuple)
 
 def main():
 	""" """
+	inside_triplet_percent_list = []
+	accs = []
 
-	label_dict = [
+	label_dict = {
 		"incorrect_alignment": 0,
 		"gt_alignment_approx": 1
-	]
+	}
+
+	all_floor_rot_errs = []
 
 	#hypotheses_dir = "/Users/johnlam/Downloads/ZinD_alignment_hypotheses_2021_06_25"
 	hypotheses_dir = "/Users/johnlam/Downloads/DGX-rendering-2021_06_25/ZinD_alignment_hypotheses_2021_06_25"
 
-	building_ids = [Path(dirpath).stem for dirpath in glob.glob(f"{hypotheses_dir}/*")]
+	building_ids = [ int(Path(dirpath).stem) for dirpath in glob.glob(f"{hypotheses_dir}/*")]
 
 	for building_id in building_ids:
 
-		floor_ids = [Path(dirpath) for dirpath in glob.glob(f"{hypotheses_dir}/{building_id}/*")]
+		if building_id != 1442:
+			continue
+
+		# if len(inside_triplet_percent_list) > 10:
+		# 	break
+
+		floor_ids = [Path(dirpath).stem for dirpath in glob.glob(f"{hypotheses_dir}/{building_id}/*")]
 		for floor_id in floor_ids:
+
+			# if not (building_id == 338 and floor_id == "floor_01"):
+			# 	continue
+
+			logger.info(f"Building {building_id}, floor {floor_id}")
 
 			floor_label_idxs = []
 			floor_sim2_json_fpaths = []
@@ -41,12 +69,10 @@ def main():
 				label_json_fpaths = glob.glob(f"{hypotheses_dir}/{building_id}/{floor_id}/{label_type}/*")
 				label_idxs = [label_idx] * len(label_json_fpaths)
 
-				sim2_json_fpaths.extend(label_json_fpaths)
+				floor_sim2_json_fpaths.extend(label_json_fpaths)
 				floor_label_idxs.extend(label_idxs)
 
 			floor_label_idxs = np.array(floor_label_idxs)
-
-			import pdb; pdb.set_trace()
 
 			# TODO: cache all of the model results beforehand (suppose we randomly pollute 8.5% of the results)
 			POLLUTION_FRAC = 0.085
@@ -54,30 +80,398 @@ def main():
 			num_floor_labels = len(floor_label_idxs)
 			idxs_to_pollute = np.random.choice(a=num_floor_labels, size=int(POLLUTION_FRAC * num_floor_labels))
 
-			floor_label_idxs[idxs_to_pollute] = 1 - floor_label_idxs[idxs_to_pollute]
+			corrupted_floor_label_idxs = copy.deepcopy(floor_label_idxs)
+			corrupted_floor_label_idxs[idxs_to_pollute] = 1 - corrupted_floor_label_idxs[idxs_to_pollute]
 
 			# for a single floor, find all of the triplets
 			two_view_reports_dict = {}
 
 			i2Ri1_dict = {}
-			i2Ui1_dict = {}
+			i2ti1_dict = {}
 
-			# check if the triplet is self consistent
+			floor_pano_ids = []
+			for sim2_json_fpath, gt_class_idx in zip(floor_sim2_json_fpaths, floor_label_idxs):
 
-			i2Ri1_consistent, i2Ui1_consistent = filter_to_cycle_consistent_edges(
-    			i2Ri1_dict,
-    			i2Ui1_dict,
-    			two_view_reports_dict,
-    			visualize=False
-    		)
+				if gt_class_idx != 1:
+					continue
 
-			# if so, admit the 3 edges to the graph
+				i1, i2 = Path(sim2_json_fpath).stem.split("_")[:2]
+				i1, i2 = int(i1), int(i2)
+
+				floor_pano_ids.append(i1)
+				floor_pano_ids.append(i2)	
+
+			for sim2_json_fpath, corrupted_class_idx, gt_class_idx in zip(floor_sim2_json_fpaths, corrupted_floor_label_idxs, floor_label_idxs):
+
+				# add if potentially corrupted label said it was a match
+				# if corrupted_class_idx != 1:
+				# 	continue
+
+				if gt_class_idx != 1:
+					continue
+
+				i1, i2 = Path(sim2_json_fpath).stem.split("_")[:2]
+				i1, i2 = int(i1), int(i2)
+
+				i2Ti1 = Sim2.from_json(json_fpath=sim2_json_fpath)
+				i2Ri1_dict[(i1,i2)] = i2Ti1.rotation
+				i2ti1_dict[(i1,i2)] = i2Ti1.translation
+
+				two_view_reports_dict[(i1,i2)] = TwoViewEstimationReport(gt_class=gt_class_idx)
+
+			#print(i2Ri1_dict.keys())
+
+			wRi_list_Rot3 = global_averaging(i2Ri1_dict)
+
+			# # have to align the pose graphs up to a Sim(3) transformation
+			# import gtsfm.utils.geometry_comparisons as comp_utils
+
+			# wRi_list_Rot3 = comp_utils.align_rotations(wRi_list_Rot3_gt, wRi_list_Rot3)
+			wRi_list = posegraph3d_to_posegraph2d(wRi_list_Rot3)
+
+			errs = []
+
+			# check if relative constraints are still satisfied w.r.t. global list
+			for sim2_json_fpath, corrupted_class_idx, gt_class_idx in zip(floor_sim2_json_fpaths, corrupted_floor_label_idxs, floor_label_idxs):
+
+				if gt_class_idx != 1:
+					continue
+
+				i1, i2 = Path(sim2_json_fpath).stem.split("_")[:2]
+				i1, i2 = int(i1), int(i2)
+
+				i2Ti1 = Sim2.from_json(json_fpath=sim2_json_fpath)
+
+				theta_deg = i2Ti1.theta_deg
+
+				wRi1 = wRi_list[i1]
+				wRi2 = wRi_list[i2]
+
+				if wRi1 is None or wRi2 is None:
+					continue
+
+				i2Ri1 = wRi2.T @ wRi1
+
+				theta_deg_est = cycle_utils.rotmat2theta_deg(i2Ri1)
+				print(f"({i1},{i2}): GT {theta_deg:.2f} vs. {theta_deg_est:.2f}")
+				#import pdb; pdb.set_trace()
+
+				# need to wrap around at 360
+				err = wrap_angle_deg(theta_deg, theta_deg_est)
+				errs.append(err)
+
+			all_floor_rot_errs.extend(errs)
+
+			# plt.hist(all_floor_rot_errs, bins=10)
+			# plt.ylabel("Counts")
+			# plt.xlabel("Absolute rotation error (degrees)")
+			# plt.show()
+
+			import pdb; pdb.set_trace()
+
+			# try greedy composition
+			# draw the edges in a top-down floor plan, visible edges
+			# use confidence value. would be good for it to be a reliable number.
+			# visualize the O(n^2k^2) graph, put it in a confusion matrix
 
 
+			# triplets = cycle_utils.extract_triplets_adjacency_list_intersection(i2Ri1_dict)
+
+			# present_in_any_triplet = np.zeros((len(floor_pano_ids)), dtype=bool)
+			# # for each node
+			# for i, node in enumerate(floor_pano_ids):
+			# 	# check if present in any triplet
+			# 	present_in_any_triplet[i] = node_present_in_any_triplet(node, triplets)
+
+			# #import pdb; pdb.set_trace()
+
+			# inside_triplet_percent = np.mean(present_in_any_triplet)
+			# logger.info(f"Building {building_id}, floor {floor_id}, % nodes in any triplet? {inside_triplet_percent:.3f}")
+
+			# inside_triplet_percent_list.append(inside_triplet_percent)
+
+
+
+			# check which triplets are self consistent. if so, admit the 3 edges to the graph
+			# i2Ri1_consistent, i2Ui1_consistent = cycle_utils.filter_to_cycle_consistent_edges(
+   #  			i2Ri1_dict,
+   #  			i2ti1_dict,
+   #  			two_view_reports_dict,
+   #  			visualize=False
+   #  		)
+
+			# # import pdb; pdb.set_trace()
+
+			# predictions = []
+			# for (i1,i2) in i2Ri1_consistent.keys():
+			# 	predictions.append(two_view_reports_dict[(i1,i2)].gt_class)
+
+			# predictions = np.array(predictions)
+			# acc = np.mean(predictions)
+			# accs.append(acc)
+			# logger.info(f"Building {building_id}, floor {floor_id}, Mean accuracy: {acc:.2f}\n")
+
+
+	plt.scatter(inside_triplet_percent_list, accs)
+	plt.show()
+
+	plt.hist(inside_triplet_percent_list, bins=50)
+	plt.xlabel("Fraction of nodes found in any triplet")
+	plt.ylabel("Counts")
+	plt.show()
+
+
+# def wrap_angle_deg(angles: np.ndarray, period: float = 360) -> np.ndarray:
+#     """Map angles (in degrees) from domain [-∞, ∞] to [0, 180). This function is
+#         the inverse of `np.unwrap`.
+#     Returns:
+#         Angles (in radians) mapped to the interval [0, 180).
+#     """
+#     # Map angles to [0, ∞].
+#     angles = np.abs(angles)
+
+#     # Calculate floor division and remainder simultaneously.
+#     divs, mods = np.divmod(angles, period)
+
+#     # Select angles which exceed specified period.
+#     angle_complement_mask = np.nonzero(divs)
+
+#     # Take set complement of `mods` w.r.t. the set [0, π].
+#     # `mods` must be nonzero, thus the image is the interval [0, π).
+#     angles[angle_complement_mask] = period - mods[angle_complement_mask]
+#     return angles
+
+def wrap_angle_deg(angle1: float, angle2: float):
+	"""
+	https://stackoverflow.com/questions/28036652/finding-the-shortest-distance-between-two-angles/28037434
+	"""
+	# mod n will wrap x to [0,n)
+	diff = ( angle2 - angle1 + 180 ) % 360 - 180
+	if diff < -180:
+		return np.absolute(diff + 360)
+	else:
+		return np.absolute(diff)
+
+
+def test_wrap_angle_deg() -> None:
+	""" """
+	angle1 = 180
+	angle2 = -180
+	orientation_err = wrap_angle_deg(angle1, angle2)
+	assert orientation_err == 0
+	
+	angle1 = -180
+	angle2 = 180
+	import pdb; pdb.set_trace()
+	orientation_err = wrap_angle_deg(angle1, angle2)
+	assert orientation_err == 0
+
+	angle1 = -45
+	angle2 = -47
+	orientation_err = wrap_angle_deg(angle1, angle2)
+	assert orientation_err == 2
+
+	angle1 = 1
+	angle2 = -1
+	orientation_err = wrap_angle_deg(angle1, angle2)
+	assert orientation_err == 2
+
+
+	angle1 = 10
+	angle2 = 11.5
+	orientation_err = wrap_angle_deg(angle1, angle2)
+	assert orientation_err == 1.5
+
+
+
+
+def posegraph2d_to_posegraph3d(wRi_list: List[Optional[np.ndarray]]) -> List[Optional[Rot3]]:
+	""" """
+	num_images = len(wRi_list)
+	wRi_list_Rot3 = [None] * num_images
+	for i, wRi in enumerate(wRi_list_Rot3):
+
+		if wRi is None:
+			continue
+
+		wRi_Rot3 = np.eye(3)
+		wRi_Rot3[:2,:2] = wRi
+
+		wRi_list_Rot3[i] = Rot3(wRi_Rot3)
+	return wRi_list_Rot3
+
+
+def posegraph3d_to_posegraph2d(wRi_list_Rot3: List[Optional[Rot3]]) -> List[Optional[np.ndarray]]:
+	""" """
+	num_images = len(wRi_list_Rot3)
+	wRi_list = [None] * num_images
+	for i, wRi_Rot3 in enumerate(wRi_list_Rot3):
+
+		if wRi_Rot3 is None:
+			continue
+
+		wRi = wRi_Rot3.matrix()[:2,:2]
+		wRi_list[i] = wRi
+	return wRi_list
+
+
+def global_averaging(i2Ri1_dict) -> List[Optional[Rot3]]:
+	"""Lift 2d rotation matrix to 3d, and then convert back to 2d"""
+	i2Ri1_dict_Rot3 = {}
+
+	max_pano_id = 0
+
+	for (i1,i2), i2Ri1 in i2Ri1_dict.items():
+
+		R = np.eye(3)
+		R[:2,:2] = i2Ri1
+
+		i2Ri1_dict_Rot3[(i1,i2)] = Rot3(R)
+
+		max_pano_id = max(i1, max_pano_id)
+		max_pano_id = max(i2, max_pano_id)
+
+	#import pdb; pdb.set_trace()
+
+	from gtsfm.averaging.rotation.shonan import ShonanRotationAveraging
+	shonan_obj = ShonanRotationAveraging()
+	num_images = max_pano_id + 1
+	
+	wRi_list_Rot3 = shonan_obj.run(num_images, i2Ri1_dict_Rot3)
+
+	return wRi_list_Rot3
+
+
+def node_present_in_any_triplet(node: int, triplets: List[Tuple[int,int,int]]):
+	""" """
+	return any([node in triplet for triplet in triplets])
+
+
+def test_node_present_in_any_triplet():
+	""" """
+	triplets = [(1,2,3),(4,5,6),(3,5,6)]
+
+	assert not node_present_in_any_triplet(8, triplets)
+	assert node_present_in_any_triplet(3, triplets)
+
+
+@dataclass(frozen=False)
+class TwoViewEstimationReport:
+
+	gt_class: int
+	R_error_deg: Optional[float] = None
+	U_error_deg: Optional[float] = None
+
+
+
+def greedily_construct_mst(i2Ri1_dict: Dict[Tuple[int,int], Rot3]) -> List[Rot3]:
+	""" Minimum spanning tree
+
+	Args:
+	    i2Ri1_dict: relative rotations
+
+	Returns:
+	    wRi_list: global rotations
+	"""
+	# find the largest connected component
+	edges = i2Ri1_dict.keys()
+
+	num_nodes = max([max(i1,i2) for i1,i2 in edges]) + 1
+
+	cc_nodes = graph_utils.get_nodes_in_largest_connected_component(edges)
+	cc_nodes = sorted(cc_nodes)
+
+	# form adjacency list
+	adj_list = defaultdict(set)
+
+	for (i1, i2), i2Ri1 in i2Ri1_dict.items():
+
+		adj_list[i1].add(i2)
+		adj_list[i2].add(i1)
+
+	wRi_list = [None] * num_nodes
+	# choose origin node
+	origin_node = cc_nodes[0]
+	wRi_list[origin_node] = Rot3()
+
+	
+	G = nx.Graph()
+	G.add_edges_from(edges)
+
+	# ignore 0th node, as we already set its global pose as the origin
+	for dst_node in cc_nodes[1:]:
+
+		# determine the path to this node from the origin. ordered from [origin_node,...,dst_node]
+		path = nx.shortest_path(G, source=origin_node, target=dst_node)
+
+		wRi = Rot3()
+		for (i1, i2) in zip(path[:-1], path[1:]):
+			# wRi = wR0 * 0R1
+			i1Ri2 = i2Ri1_dict[i1, i2].inverse()
+			wRi = wRi.compose(i1Ri2)
+
+		wRi_list[dst_node] = wRi
+
+	return wRi_list
+
+
+def test_greedily_construct_mst():
+	"""
+	Graph topology:
+
+              | 2     | 3
+              o-- ... o-- 
+              .       .
+              .       .
+	|         |       |
+	o-- ... --o     --o
+	0         1       4
+
+	"""
+	wRi_list_gt = [
+		Rot3(), # 0
+		Rot3.Rz(np.deg2rad(90)), # 1
+		Rot3(), # 2
+		Rot3(), # 3
+		Rot3.Rz(np.deg2rad(90))# 4
+	]
+
+	i2Ri1_dict = {}
+	for i1 in range(4):
+		i2 = i1 + 1
+		i2Ri1_dict[(i1,i2)] = wRi_list_gt[i2].between(wRi_list_gt[i1])
+
+	print(i2Ri1_dict)
+
+	wRi_list_greedy = greedily_construct_mst(i2Ri1_dict)
+
+	# expected angles
+	wRi_list_euler_deg_exp = [
+		[0, 0,  0],
+		[0, 0, 90],
+		[0, 0,  0],
+		[0, 0,  0],
+		[0, 0, 90]
+	]
+	wRi_list_euler_deg_est = [ np.rad2deg(wRi.xyz()).tolist() for wRi in wRi_list_greedy]
+	assert wRi_list_euler_deg_exp == wRi_list_euler_deg_est
+
+	import pdb; pdb.set_trace()
+	# cast to a 2d problem
+	wRi_list_Rot3_shonan = global_averaging(i2Ri1_dict)
+	wRi_list_shonan = posegraph3d_to_posegraph2d(wRi_list_Rot3_shonan)
+	
 
 
 
 if __name__ == "__main__":
-	main()
+	""" """
+
+	test_greedily_construct_mst()
+
+	#main()
+	#test_node_present_in_any_triplet()
+
+	#test_wrap_angle_deg()
 
 
