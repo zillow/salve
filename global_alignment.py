2b743c5b843c97832371002c8c151028ce2cd582
John Lambert <johnlam@zillowgroup.com>
2021-07-06 14:00:22 -0400

    use 2d shonan averaging

--- global_alignment.py
+++ global_alignment.py
@@ -12,12 +12,13 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List, NamedTuple, Optional, Tuple
 
+import gtsam
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from argoverse.utils.json_utils import read_json_file
 from argoverse.utils.sim2 import Sim2
-from gtsam import Rot3
+from gtsam import Rot2, Rot3
 
 import cycle_consistency as cycle_utils
 
@@ -26,6 +27,7 @@ import gtsfm.utils.logger as logger_utils
 
 logger = logger_utils.get_logger()
 
+from vis_depth import rotmat2d
 
 # class TwoViewEdge(NamedTuple)
 
@@ -121,13 +123,21 @@ def main():
 
 			#print(i2Ri1_dict.keys())
 
-			wRi_list_Rot3 = global_averaging(i2Ri1_dict)
+			method = "shonan" # "greedy"
+			if method == "shonan":
+				import pdb; pdb.set_trace()
+				wRi_list = globalaveraging2d(i2Ri1_dict)
+				# wRi_list_Rot3 = global_averaging(i2Ri1_dict)
+				# #print(wRi_list_Rot3)
 
-			# # have to align the pose graphs up to a Sim(3) transformation
-			# import gtsfm.utils.geometry_comparisons as comp_utils
+				# # # have to align the pose graphs up to a Sim(3) transformation
+				# # import gtsfm.utils.geometry_comparisons as comp_utils
 
-			# wRi_list_Rot3 = comp_utils.align_rotations(wRi_list_Rot3_gt, wRi_list_Rot3)
-			wRi_list = posegraph3d_to_posegraph2d(wRi_list_Rot3)
+				# # wRi_list_Rot3 = comp_utils.align_rotations(wRi_list_Rot3_gt, wRi_list_Rot3)
+				# wRi_list = posegraph3d_to_posegraph2d(wRi_list_Rot3)
+
+			elif method == "greedy":
+				wRi_list = greedily_construct_mst(i2Ri1_dict)
 
 			errs = []
 
@@ -161,6 +171,8 @@ def main():
 				errs.append(err)
 
 			all_floor_rot_errs.extend(errs)
+
+			print("Mean error: ", np.mean(all_floor_rot_errs))
 
 			# plt.hist(all_floor_rot_errs, bins=10)
 			# plt.ylabel("Counts")
@@ -315,8 +327,13 @@ def posegraph3d_to_posegraph2d(wRi_list_Rot3: List[Optional[Rot3]]) -> List[Opti
 	return wRi_list
 
 
-def global_averaging(i2Ri1_dict) -> List[Optional[Rot3]]:
-	"""Lift 2d rotation matrix to 3d, and then convert back to 2d"""
+def global_averaging(i2Ri1_dict: Dict[Tuple[int,int], np.ndarray]) -> List[Optional[Rot3]]:
+	"""Lift 2d rotation matrix to 3d, then run Shonan rotation averaging.
+
+	
+	Args:
+	    i2Ri1_dict: mapping from image pair indices to 2x2 array representing 2d rotation matrix
+	"""
 	i2Ri1_dict_Rot3 = {}
 
 	max_pano_id = 0
@@ -336,6 +353,8 @@ def global_averaging(i2Ri1_dict) -> List[Optional[Rot3]]:
 	from gtsfm.averaging.rotation.shonan import ShonanRotationAveraging
 	shonan_obj = ShonanRotationAveraging()
 	num_images = max_pano_id + 1
+
+	print(i2Ri1_dict_Rot3)
 	
 	wRi_list_Rot3 = shonan_obj.run(num_images, i2Ri1_dict_Rot3)
 
@@ -364,14 +383,14 @@ class TwoViewEstimationReport:
 
 
 
-def greedily_construct_mst(i2Ri1_dict: Dict[Tuple[int,int], Rot3]) -> List[Rot3]:
+def greedily_construct_mst(i2Ri1_dict: Dict[Tuple[int,int], Rot3]) -> List[np.ndarray]:
 	""" Minimum spanning tree
 
 	Args:
 	    i2Ri1_dict: relative rotations
 
 	Returns:
-	    wRi_list: global rotations
+	    wRi_list: global 2d rotations
 	"""
 	# find the largest connected component
 	edges = i2Ri1_dict.keys()
@@ -392,7 +411,7 @@ def greedily_construct_mst(i2Ri1_dict: Dict[Tuple[int,int], Rot3]) -> List[Rot3]
 	wRi_list = [None] * num_nodes
 	# choose origin node
 	origin_node = cc_nodes[0]
-	wRi_list[origin_node] = Rot3()
+	wRi_list[origin_node] = np.eye(2)
 
 	
 	G = nx.Graph()
@@ -404,11 +423,11 @@ def greedily_construct_mst(i2Ri1_dict: Dict[Tuple[int,int], Rot3]) -> List[Rot3]
 		# determine the path to this node from the origin. ordered from [origin_node,...,dst_node]
 		path = nx.shortest_path(G, source=origin_node, target=dst_node)
 
-		wRi = Rot3()
+		wRi = np.eye(2)
 		for (i1, i2) in zip(path[:-1], path[1:]):
 			# wRi = wR0 * 0R1
-			i1Ri2 = i2Ri1_dict[i1, i2].inverse()
-			wRi = wRi.compose(i1Ri2)
+			i1Ri2 = i2Ri1_dict[i1, i2].T # use inverse
+			wRi = wRi @ i1Ri2
 
 		wRi_list[dst_node] = wRi
 
@@ -428,48 +447,161 @@ def test_greedily_construct_mst():
 	0         1       4
 
 	"""
+	# ground truth 2d rotations
 	wRi_list_gt = [
-		Rot3(), # 0
-		Rot3.Rz(np.deg2rad(90)), # 1
-		Rot3(), # 2
-		Rot3(), # 3
-		Rot3.Rz(np.deg2rad(90))# 4
+		rotmat2d(0),
+		rotmat2d(90),
+		rotmat2d(0),
+		rotmat2d(0),
+		rotmat2d(90)
+		# Rot3(), # 0
+		# Rot3.Rz(np.deg2rad(90)), # 1
+		# Rot3(), # 2
+		# Rot3(), # 3
+		# Rot3.Rz(np.deg2rad(90))# 4
 	]
 
+	
 	i2Ri1_dict = {}
 	for i1 in range(4):
 		i2 = i1 + 1
-		i2Ri1_dict[(i1,i2)] = wRi_list_gt[i2].between(wRi_list_gt[i1])
+		wRi2 = wRi_list_gt[i2]
+		wRi1 = wRi_list_gt[i1]
+		i2Ri1_dict[(i1,i2)] = wRi2.T @ wRi1
 
-	print(i2Ri1_dict)
+	for (i1, i2), i2Ri1 in i2Ri1_dict.items():
+		print(f"EDGE_SE2 {i1} {i2} 0 0 {cycle_utils.rotmat2theta_deg(i2Ri1)}")
 
 	wRi_list_greedy = greedily_construct_mst(i2Ri1_dict)
 
 	# expected angles
 	wRi_list_euler_deg_exp = [
-		[0, 0,  0],
-		[0, 0, 90],
-		[0, 0,  0],
-		[0, 0,  0],
-		[0, 0, 90]
+		 0,
+		90,
+		 0,
+		 0,
+		90,
 	]
-	wRi_list_euler_deg_est = [ np.rad2deg(wRi.xyz()).tolist() for wRi in wRi_list_greedy]
+	#wRi_list_euler_deg_est = [ np.rad2deg(wRi.xyz()).tolist() for wRi in wRi_list_greedy]
+	wRi_list_euler_deg_est = [ cycle_utils.rotmat2theta_deg(wRi) for wRi in wRi_list_greedy]
 	assert wRi_list_euler_deg_exp == wRi_list_euler_deg_est
 
-	import pdb; pdb.set_trace()
-	# cast to a 2d problem
-	wRi_list_Rot3_shonan = global_averaging(i2Ri1_dict)
-	wRi_list_shonan = posegraph3d_to_posegraph2d(wRi_list_Rot3_shonan)
-	
 
+	wRi_list_shonan = globalaveraging2d(i2Ri1_dict)
+
+	wRi_list_shonan_est = [ cycle_utils.rotmat2theta_deg(wRi) for wRi in wRi_list_shonan]
+
+	# Note that:
+	# 360 - 125.812 =  234.188
+	# 234.188 - 144.188 = 90.0
+	wRi_list_shonan_exp = [-125.81, 144.18, -125.81, -125.81, 144.18]
+	assert np.allclose(wRi_list_shonan_exp, wRi_list_shonan_est, atol=0.01)
+
+
+
+	# # cast to a 2d problem
+	# wRi_list_Rot3_shonan = global_averaging(i2Ri1_dict)
+	# wRi_list_shonan = posegraph3d_to_posegraph2d(wRi_list_Rot3_shonan)
+	
+	# wRi_list_shonan_est = [ cycle_utils.rotmat2theta_deg(wRi) for wRi in wRi_list_shonan]
+
+	# # corresponds to 110.5 and 200.4 degrees (as if 0 and 90 degrees)
+	# wRi_list_shonan_exp = [110.52, -159.61, 110.52, 110.52, -159.61]
+	# assert np.allclose(wRi_list_shonan_exp, wRi_list_shonan_est, atol=0.01)
+
+
+def globalaveraging2d_consecutive_ordering(i2Ri1_dict: Dict[Tuple[int,int],np.ndarray]) -> List[np.ndarray]:
+	""" """
+	input_file = "shonan_input.g2o"
+	with open(input_file, "w") as f:
+		x = 0
+		y = 0
+		for (i1,i2), i2Ri1 in i2Ri1_dict.items():
+			theta_deg = cycle_utils.rotmat2theta_deg(i2Ri1)
+			theta_rad = np.deg2rad(theta_deg)
+			f.write(f"EDGE_SE2 {i2} {i1} {x} {y} {theta_rad} 1.000000 0.000000 0.000000 1.000000 0.000000 1.000000\n")
+
+	shonan = gtsam.ShonanAveraging2(input_file)
+	if shonan.nrUnknowns() == 0:
+		raise ValueError("No 2D pose constraints found, try -d 3.")
+	initial = shonan.initializeRandomly()
+	rotations, _ = shonan.run(initial, 2, 10)
+
+	wRi_list = [ rotations.atRot2(j).matrix() for j in range(rotations.size()) ]
+	return wRi_list
+
+
+def globalaveraging2d(i2Ri1_dict: Dict[Tuple[int, int], Optional[np.ndarray]]) -> List[Optional[np.ndarray]]:
+	"""Run the rotation averaging on a connected graph with arbitrary keys, where each key is a image/pose index.
+	Note: run() functions as a wrapper that re-orders keys to prepare a graph w/ N keys ordered [0,...,N-1].
+	All input nodes must belong to a single connected component, in order to obtain an absolute pose for each
+	camera in a single, global coordinate frame.
+	Args:
+	num_images: number of images. Since we have one pose per image, it is also the number of poses.
+	i2Ri1_dict: relative rotations for each image pair-edge as dictionary (i1, i2): i2Ri1.
+	Returns:
+	Global rotations for each camera pose, i.e. wRi, as a list. The number of entries in the list is
+	`num_images`. The list may contain `None` where the global rotation could not be computed (either
+	underconstrained system or ill-constrained system), or where the camera pose had no valid observation
+	in the input to run().
+	"""
+	edges = i2Ri1_dict.keys()
+	num_images = max([max(i1,i2) for i1,i2 in edges]) + 1
+
+	connected_nodes = set()
+	for (i1, i2) in i2Ri1_dict.keys():
+		connected_nodes.add(i1)
+		connected_nodes.add(i2)
+
+	connected_nodes = sorted(list(connected_nodes))
+
+	# given original index, this map gives back a new temporary index, starting at 0
+	reordered_idx_map = {}
+	for (new_idx, i) in enumerate(connected_nodes):
+		reordered_idx_map[i] = new_idx
+
+	# now, map the original indices to reordered indices
+	i2Ri1_dict_reordered = {}
+	for (i1, i2), i2Ri1 in i2Ri1_dict.items():
+		i1_ = reordered_idx_map[i1]
+		i2_ = reordered_idx_map[i2]
+		i2Ri1_dict_reordered[(i1_, i2_)] = i2Ri1
+
+	wRi_list_subset = globalaveraging2d_consecutive_ordering(
+		i2Ri1_dict=i2Ri1_dict_reordered
+	)
+
+	wRi_list = [None] * num_images
+	for remapped_i, original_i in enumerate(connected_nodes):
+		wRi_list[original_i] = wRi_list_subset[remapped_i]
+
+	return wRi_list
+
+
+
+
+
+# def test_shonanaveraging2():
+# 	""" """
+	# input_file = "shonanaveraging2_example.g2o"
+
+# 	# # Load 2D toy example
+	# lmParams = gtsam.LevenbergMarquardtParams.CeresDefaults()
+	# # lmParams.setVerbosityLM("SUMMARY")
+	# g2oFile = gtsam.findExampleDataFile("noisyToyGraph.txt")
+	# parameters = gtsam.ShonanAveragingParameters2(lmParams)
+	# shonan = gtsam.ShonanAveraging2(g2oFile, parameters)
+	# self.assertAlmostEqual(4, shonan.nrUnknowns())
 
 
 if __name__ == "__main__":
 	""" """
 
-	test_greedily_construct_mst()
+	#test_greedily_construct_mst()
 
-	#main()
+	#test_shonanaveraging2()
+
+	main()
 	#test_node_present_in_any_triplet()
 
 	#test_wrap_angle_deg()
