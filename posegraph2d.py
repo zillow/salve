9b4461d92846e163a8bbb9724cb5d4da586f9302
John Lambert <johnlam@zillowgroup.com>
2021-07-08 16:49:30 -0400

    add unit tests on abs and rel rotation PG error

--- posegraph2d.py
+++ posegraph2d.py
@@ -10,7 +10,7 @@ from argoverse.utils.sim2 import Sim2
 from gtsam import Rot3
 
 from pano_data import FloorData, PanoData, generate_Sim2_from_floorplan_transform
-
+from vis_depth import rotmat2d
 
 class PoseGraph2d(NamedTuple):
     """Pose graph for a single floor.
@@ -42,16 +42,18 @@ class PoseGraph2d(NamedTuple):
 
     @classmethod
     def from_wRi_list(cls, wRi_list: List[Rot3], building_id: int, floor_id: str) -> "PoseGraph2d":
-        """ """
-        import pdb; pdb.set_trace()
+        """
 
+        Fill other pano metadata with dummy values. Alternatively, could populate them from the GT pose graph.
+        """
+        nodes = {}
         for i, wRi in enumerate(wRi_list):
             if wRi is None:
                 continue
 
             nodes[i] = PanoData(
                 id=i,
-                global_SIM2_local = Sim2(R=wRi, t=np.zeros(3), s=1.0),
+                global_Sim2_local = Sim2(R=wRi, t=np.zeros(2), s=1.0),
                 room_vertices_local_2d = np.zeros((0,2)),
                 image_path="",
                 label="",
@@ -68,7 +70,7 @@ class PoseGraph2d(NamedTuple):
         pass
 
 
-    def measure_avg_abs_rotation_err(gt_floor_pg: "PoseGraph2d") -> float:
+    def measure_avg_abs_rotation_err(self, gt_floor_pg: "PoseGraph2d") -> float:
         """Measure how the absolute poses satisfy the individual binary measurement constraints.
 
         If `self` is the estimate, then we measure our error w.r.t. GT argument.
@@ -94,8 +96,122 @@ class PoseGraph2d(NamedTuple):
             errs.append(err)
 
         mean_err = np.mean(errs)
-        print(f"Mean abs rot. error: {mean_err:.2f}. Estimated rotation for {len(self.nodes)} of {len(self.gt_floor_pg.nodes)} GT panos.")
+        print(f"Mean absolute rot. error: {mean_err:.2f}. Estimated rotation for {len(self.nodes)} of {len(gt_floor_pg.nodes)} GT panos.")
         return mean_err
+
+
+    def measure_avg_rel_rotation_err(self, gt_floor_pg: "PoseGraph2d", gt_edges=List[Tuple[int,int]]) -> float:
+        """
+
+        Args:
+            gt_edges: list of (i1,i2) pairs representing panorama pairs where a WDO is found closeby between the two
+        """
+        errs = []
+        for (i1,i2) in gt_edges:
+
+            wTi1_gt = gt_floor_pg.nodes[i1].global_Sim2_local
+            wTi2_gt = gt_floor_pg.nodes[i2].global_Sim2_local
+            i2Ti1_gt = wTi2_gt.inverse().compose(wTi1_gt)
+
+            wTi1 = self.nodes[i1].global_Sim2_local
+            wTi2 = self.nodes[i2].global_Sim2_local
+            i2Ti1 = wTi2.inverse().compose(wTi1)
+
+            theta_deg_est = i2Ti1.theta_deg
+            theta_deg_gt = i2Ti1_gt.theta_deg
+
+            print(f"Pano pair ({i1},{i2}): GT {theta_deg_gt:.2f} vs. {theta_deg_est:.2f}")
+
+            # need to wrap around at 360
+            err = wrap_angle_deg(theta_deg_gt, theta_deg_est)
+            errs.append(err)
+
+        mean_err = np.mean(errs)
+        print(f"Mean relative rot. error: {mean_err:.2f}. Estimated rotation for {len(self.nodes)} of {len(gt_floor_pg.nodes)} GT panos.")
+        return mean_err
+
+
+def test_measure_avg_abs_rotation_err() -> None:
+    """
+    Create a dummy scenario, to make sure absolute rotation errors are evaluated properly.
+
+    GT rotation graph:
+
+      | 1
+    --o
+      | .
+      .   .
+      .     .
+      |       |
+      o-- ... o--
+    0          2
+    """
+    building_id = "000"
+    floor_id = "floor_01"
+
+    wRi_list = [
+        rotmat2d(-5),
+        rotmat2d(-95),
+        rotmat2d(0)
+    ]
+    
+    est_floor_pose_graph = PoseGraph2d.from_wRi_list(wRi_list, building_id, floor_id)
+
+    wRi_list_gt = [
+        rotmat2d(0),
+        rotmat2d(-90),
+        rotmat2d(0)
+    ]
+    gt_floor_pose_graph = PoseGraph2d.from_wRi_list(wRi_list_gt, building_id, floor_id)
+
+    mean_abs_rot_err = est_floor_pose_graph.measure_avg_abs_rotation_err(gt_floor_pg=gt_floor_pose_graph)
+
+    assert np.isclose(mean_abs_rot_err, 10/3, atol=1e-3)
+
+    # TODO: compute if some pano poses are unestimated (i.e. None)
+
+
+def test_measure_avg_rel_rotation_err() -> None:
+    """
+    Create a dummy scenario, to make sure relative rotation errors are evaluated properly.
+
+    GT rotation graph:
+
+      | 1
+    --o
+      | .
+      .   .
+      .     .
+      |       |
+      o-- ... o--
+    0          2
+    """
+    building_id = "000"
+    floor_id = "floor_01"
+
+    wRi_list = [
+        rotmat2d(-5),
+        rotmat2d(-95),
+        rotmat2d(0)
+    ]
+    est_floor_pose_graph = PoseGraph2d.from_wRi_list(wRi_list, building_id, floor_id)
+
+    wRi_list_gt = [
+        rotmat2d(0),
+        rotmat2d(-90),
+        rotmat2d(0)
+    ]
+    gt_floor_pose_graph = PoseGraph2d.from_wRi_list(wRi_list_gt, building_id, floor_id)
+
+    gt_edges = [(0,1)]
+    mean_rel_rot_err = est_floor_pose_graph.measure_avg_rel_rotation_err(gt_floor_pg=gt_floor_pose_graph, gt_edges=gt_edges)
+    # both are incorrect by the same amount
+    assert mean_rel_rot_err == 0
+
+    gt_edges = [(0,1),(1,2),(0,2)]
+    mean_rel_rot_err = est_floor_pose_graph.measure_avg_rel_rotation_err(gt_floor_pg=gt_floor_pose_graph, gt_edges=gt_edges)
+    assert np.isclose(mean_rel_rot_err, 10/3, atol=1e-3)
+    
 
 
 def wrap_angle_deg(angle1: float, angle2: float):
@@ -119,9 +235,6 @@ def test_wrap_angle_deg() -> None:
 
     angle1 = -180
     angle2 = 180
-    import pdb
-
-    pdb.set_trace()
     orientation_err = wrap_angle_deg(angle1, angle2)
     assert orientation_err == 0
 
@@ -195,7 +308,10 @@ def get_gt_pose_graph(building_id: int, floor_id: str, raw_dataset_dir: str) -> 
 if __name__ == "__main__":
     """ """
     raw_dataset_dir = "/Users/johnlam/Downloads/2021_05_28_Will_amazon_raw"
-    export_dataset_pose_graphs(raw_dataset_dir)
+    #export_dataset_pose_graphs(raw_dataset_dir)
+
+    test_measure_avg_rel_rotation_err()
+    #test_measure_avg_abs_rotation_err()
 
 
 
