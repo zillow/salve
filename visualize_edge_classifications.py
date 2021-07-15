ef2d5550e72743ddd8bdf992ae404b0f0c29b686
John Lambert <johnlam@zillowgroup.com>
2021-07-15 16:13:41 -0600

    reformat python black

--- visualize_edge_classifications.py
+++ visualize_edge_classifications.py
@@ -1,4 +1,3 @@
-
 import glob
 from collections import defaultdict
 from pathlib import Path
@@ -12,173 +11,169 @@ from pr_utils import assign_tp_fp_fn_tn
 
 
 class EdgeClassification(NamedTuple):
-	"""
-	i1 and i2 are panorama id's
-	"""
-	i1: int
-	i2: int
-	prob: float
-	y_hat: int
-	y_true: int
-	pair_idx: int
-	wdo_pair_uuid: str
-	configuration: str
+    """
+    i1 and i2 are panorama id's
+    """
+
+    i1: int
+    i2: int
+    prob: float
+    y_hat: int
+    y_true: int
+    pair_idx: int
+    wdo_pair_uuid: str
+    configuration: str
 
 
-def get_edge_classifications_from_serialized_preds(serialized_preds_json_dir: str) -> Dict[Tuple[str,str], List[EdgeClassification]]:
-	"""
+def get_edge_classifications_from_serialized_preds(
+    serialized_preds_json_dir: str,
+) -> Dict[Tuple[str, str], List[EdgeClassification]]:
+    """
 
-	Args:
-	    serialized_preds_json_dir: 
+    Args:
+        serialized_preds_json_dir:
 
-	Returns:
-	    floor_edgeclassifications_dict
-	"""
-	floor_edgeclassifications_dict = defaultdict(list)
+    Returns:
+        floor_edgeclassifications_dict
+    """
+    floor_edgeclassifications_dict = defaultdict(list)
 
-	json_fpaths = glob.glob(f"{serialized_preds_json_dir}/batch*.json")
-	for json_fpath in json_fpaths:
+    json_fpaths = glob.glob(f"{serialized_preds_json_dir}/batch*.json")
+    for json_fpath in json_fpaths:
 
-		json_data = read_json_file(json_fpath)
-		y_hat_list = json_data["y_hat"]
-		y_true_list = json_data["y_true"]
-		y_hat_prob_list = json_data["y_hat_probs"]
-		fp0_list = json_data["fp0"]
-		fp1_list = json_data["fp1"]
-		fp2_list = json_data["fp2"]
-		fp3_list = json_data["fp3"]
+        json_data = read_json_file(json_fpath)
+        y_hat_list = json_data["y_hat"]
+        y_true_list = json_data["y_true"]
+        y_hat_prob_list = json_data["y_hat_probs"]
+        fp0_list = json_data["fp0"]
+        fp1_list = json_data["fp1"]
+        fp2_list = json_data["fp2"]
+        fp3_list = json_data["fp3"]
 
-		for y_hat, y_true, y_hat_prob, fp0, fp1, fp2, fp3 in zip(y_hat_list, y_true_list, y_hat_prob_list, fp0_list, fp1_list, fp2_list, fp3_list):
-			i1 = int(Path(fp0).stem.split('_')[-1])
-			i2 = int(Path(fp1).stem.split('_')[-1])
-			building_id = Path(fp0).parent.stem
+        for y_hat, y_true, y_hat_prob, fp0, fp1, fp2, fp3 in zip(
+            y_hat_list, y_true_list, y_hat_prob_list, fp0_list, fp1_list, fp2_list, fp3_list
+        ):
+            i1 = int(Path(fp0).stem.split("_")[-1])
+            i2 = int(Path(fp1).stem.split("_")[-1])
+            building_id = Path(fp0).parent.stem
 
-			s = Path(fp0).stem.find('floor_')
-			e = Path(fp0).stem.find('_partial')
-			floor_id = Path(fp0).stem[s:e]
+            s = Path(fp0).stem.find("floor_")
+            e = Path(fp0).stem.find("_partial")
+            floor_id = Path(fp0).stem[s:e]
 
-			pair_idx = Path(fp0).stem.split('_')[1]
+            pair_idx = Path(fp0).stem.split("_")[1]
 
-			is_identity = "identity" in Path(fp0).stem
-			configuration = "identity" if is_identity else "rotated"
+            is_identity = "identity" in Path(fp0).stem
+            configuration = "identity" if is_identity else "rotated"
 
-			k = Path(fp0).stem.split('___')[1].find(f'_{configuration}')
-			assert k != -1
+            k = Path(fp0).stem.split("___")[1].find(f"_{configuration}")
+            assert k != -1
 
-			wdo_pair_uuid = Path(fp0).stem.split('___')[1][:k]
-			assert any([wdo_type in wdo_pair_uuid for wdo_type in ['door', 'window', 'opening']])
+            wdo_pair_uuid = Path(fp0).stem.split("___")[1][:k]
+            assert any([wdo_type in wdo_pair_uuid for wdo_type in ["door", "window", "opening"]])
 
-			floor_edgeclassifications_dict[(building_id, floor_id)] += [
-				EdgeClassification(
-					i1=i1,
-					i2=i2,
-					prob=y_hat_prob,
-					y_hat=y_hat,
-					y_true=y_true,
-					pair_idx=pair_idx,
-					wdo_pair_uuid=wdo_pair_uuid,
-					configuration=configuration
-				)
-			]
-	return floor_edgeclassifications_dict
+            floor_edgeclassifications_dict[(building_id, floor_id)] += [
+                EdgeClassification(
+                    i1=i1,
+                    i2=i2,
+                    prob=y_hat_prob,
+                    y_hat=y_hat,
+                    y_true=y_true,
+                    pair_idx=pair_idx,
+                    wdo_pair_uuid=wdo_pair_uuid,
+                    configuration=configuration,
+                )
+            ]
+    return floor_edgeclassifications_dict
 
 
 def vis_edge_classifications(serialized_preds_json_dir: str, raw_dataset_dir: str) -> None:
-	""" """
-	floor_edgeclassifications_dict = get_edge_classifications_from_serialized_preds(serialized_preds_json_dir)
+    """ """
+    floor_edgeclassifications_dict = get_edge_classifications_from_serialized_preds(serialized_preds_json_dir)
 
-	color_dict = {
-		'TP': 'green',
-		'FP': 'red',
-		'FN': 'orange',
-		'TN': 'blue'
-	}
+    color_dict = {"TP": "green", "FP": "red", "FN": "orange", "TN": "blue"}
 
-	# loop over each building and floor
-	for (building_id, floor_id), measurements in floor_edgeclassifications_dict.items():
+    # loop over each building and floor
+    for (building_id, floor_id), measurements in floor_edgeclassifications_dict.items():
 
-		# if building_id != '1490': # '1394':# '1635':
-		# 	continue
+        # if building_id != '1490': # '1394':# '1635':
+        # 	continue
 
-		print(f"On building {building_id}, {floor_id}")
-		gt_floor_pose_graph = get_gt_pose_graph(building_id, floor_id, raw_dataset_dir)
+        print(f"On building {building_id}, {floor_id}")
+        gt_floor_pose_graph = get_gt_pose_graph(building_id, floor_id, raw_dataset_dir)
 
-		# gather all of the edge classifications
-		y_hat = np.array([m.y_hat for m in measurements])
-		y_true = np.array([m.y_true for m in measurements])
+        # gather all of the edge classifications
+        y_hat = np.array([m.y_hat for m in measurements])
+        y_true = np.array([m.y_true for m in measurements])
 
-		# classify into TPs, FPs, FNs, TNs
-		is_TP, is_FP, is_FN, is_TN = assign_tp_fp_fn_tn(y_true, y_pred=y_hat)
-		for m, is_tp, is_fp, is_fn, is_tn in zip(measurements, is_TP, is_FP, is_FN, is_TN):
+        # classify into TPs, FPs, FNs, TNs
+        is_TP, is_FP, is_FN, is_TN = assign_tp_fp_fn_tn(y_true, y_pred=y_hat)
+        for m, is_tp, is_fp, is_fn, is_tn in zip(measurements, is_TP, is_FP, is_FN, is_TN):
 
-			# then render the edges
-			if is_tp:
-				color = color_dict['TP']
-				#gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
-				print(f"\tFP: ({m.i1},{m.i2}) for pair {m.pair_idx}")
+            # then render the edges
+            if is_tp:
+                color = color_dict["TP"]
+                # gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
+                print(f"\tFP: ({m.i1},{m.i2}) for pair {m.pair_idx}")
 
-			elif is_fp:
-				color = color_dict['FP']
-				
-			elif is_fn:
-				color = color_dict['FN']
-				#gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
+            elif is_fp:
+                color = color_dict["FP"]
 
-			elif is_tn:
-				color = color_dict['TN']
-				gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
+            elif is_fn:
+                color = color_dict["FN"]
+                # gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
 
-			# if m.i1 or m.i2 not in gt_floor_pose_graph.nodes:
-			# 	import pdb; pdb.set_trace()
-			
+            elif is_tn:
+                color = color_dict["TN"]
+                gt_floor_pose_graph.draw_edge(m.i1, m.i2, color)
 
-		#import pdb; pdb.set_trace()
-		# render the pose graph first
-		gt_floor_pose_graph.render_estimated_layout()
-		#continue
+            # if m.i1 or m.i2 not in gt_floor_pose_graph.nodes:
+            # 	import pdb; pdb.set_trace()
 
+        # import pdb; pdb.set_trace()
+        # render the pose graph first
+        gt_floor_pose_graph.render_estimated_layout()
+        # continue
 
 
 def run_incremental_reconstruction(hypotheses_save_root: str, serialized_preds_json_dir: str, raw_dataset_dir: str):
-	""" """
-	floor_edgeclassifications_dict = get_edge_classifications_from_serialized_preds(serialized_preds_json_dir)
+    """ """
+    floor_edgeclassifications_dict = get_edge_classifications_from_serialized_preds(serialized_preds_json_dir)
 
-	# loop over each building and floor
-	# for each building/floor tuple
-	for (building_id, floor_id), measurements in floor_edgeclassifications_dict.items():
+    # loop over each building and floor
+    # for each building/floor tuple
+    for (building_id, floor_id), measurements in floor_edgeclassifications_dict.items():
 
-		print(f"On building {building_id}, {floor_id}")
+        print(f"On building {building_id}, {floor_id}")
 
-		for m in measurements:
-			# find all of the predictions where pred class is 1
-			if m.y_hat != 1:
-				continue
+        for m in measurements:
+            # find all of the predictions where pred class is 1
+            if m.y_hat != 1:
+                continue
 
-			import pdb; pdb.set_trace()
+            import pdb
 
-			#look up the associated Sim(2) file for this prediction, by looping through the pair idxs again
-			label_dirname = "gt_alignment_approx" if m.y_true else "incorrect_alignment"
-			fpaths = glob.glob(f"{hypotheses_save_root}/{building_id}/{floor_id}/{label_dirname}/{m.i1}_{m.i2}__{m.wdo_pair_uuid}_{m.configuration}.json")
-			assert len(fpaths) == 1
-			i2Ti1 = Sim2.from_json(fpaths[0])
+            pdb.set_trace()
 
-			
+            # look up the associated Sim(2) file for this prediction, by looping through the pair idxs again
+            label_dirname = "gt_alignment_approx" if m.y_true else "incorrect_alignment"
+            fpaths = glob.glob(
+                f"{hypotheses_save_root}/{building_id}/{floor_id}/{label_dirname}/{m.i1}_{m.i2}__{m.wdo_pair_uuid}_{m.configuration}.json"
+            )
+            assert len(fpaths) == 1
+            i2Ti1 = Sim2.from_json(fpaths[0])
 
 
 if __name__ == "__main__":
-	""" """
-	#serialized_preds_json_dir = "/Users/johnlam/Downloads/2021_07_13_binary_model_edge_classifications"
-	#serialized_preds_json_dir = "/Users/johnlam/Downloads/2021_07_13_edge_classifications_fixed_argmax_bug/2021_07_13_edge_classifications_fixed_argmax_bug"
-	serialized_preds_json_dir = "/Users/johnlam/Downloads/ZinD_trained_models_2021_06_25/2021_06_28_07_01_26/2021_07_15_serialized_edge_classifications/2021_07_15_serialized_edge_classifications"
+    """ """
+    # serialized_preds_json_dir = "/Users/johnlam/Downloads/2021_07_13_binary_model_edge_classifications"
+    # serialized_preds_json_dir = "/Users/johnlam/Downloads/2021_07_13_edge_classifications_fixed_argmax_bug/2021_07_13_edge_classifications_fixed_argmax_bug"
+    serialized_preds_json_dir = "/Users/johnlam/Downloads/ZinD_trained_models_2021_06_25/2021_06_28_07_01_26/2021_07_15_serialized_edge_classifications/2021_07_15_serialized_edge_classifications"
 
-	raw_dataset_dir = "/Users/johnlam/Downloads/ZInD_release/complete_zind_paper_final_localized_json_6_3_21"
-	#raw_dataset_dir = "/Users/johnlam/Downloads/2021_05_28_Will_amazon_raw"
-	#vis_edge_classifications(serialized_preds_json_dir, raw_dataset_dir)
+    raw_dataset_dir = "/Users/johnlam/Downloads/ZInD_release/complete_zind_paper_final_localized_json_6_3_21"
+    # raw_dataset_dir = "/Users/johnlam/Downloads/2021_05_28_Will_amazon_raw"
+    # vis_edge_classifications(serialized_preds_json_dir, raw_dataset_dir)
 
-	hypotheses_save_root = "/Users/johnlam/Downloads/ZinD_alignment_hypotheses_2021_07_14_v3_w_wdo_idxs"
+    hypotheses_save_root = "/Users/johnlam/Downloads/ZinD_alignment_hypotheses_2021_07_14_v3_w_wdo_idxs"
 
-
-	run_incremental_reconstruction(hypotheses_save_root, serialized_preds_json_dir, raw_dataset_dir)
-
-
-
+    run_incremental_reconstruction(hypotheses_save_root, serialized_preds_json_dir, raw_dataset_dir)
