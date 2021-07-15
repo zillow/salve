9c178bb70134079b7135c459af33c21ecc723e6b
John Lambert <johnlam@zillowgroup.com>
2021-07-14 20:48:35 -0600

    add new dataset fpath parser for new fpath format

--- zind_data.py
+++ zind_data.py
@@ -1,3 +1,8 @@
+
+"""
+Dataset that reads ZinD data, and feeds it to a Pytorch dataloader.
+"""
+
 import glob
 import logging
 from collections import defaultdict
@@ -15,16 +20,17 @@ def pair_idx_from_fpath(fpath: str) -> int:
     """ """
     fname_stem = Path(fpath).stem
 
-    parts = fname_stem.split("_")
+    parts = fname_stem.split("___")[0].split('_')
     return int(parts[1])
 
 
 def test_pair_idx_from_fpath() -> None:
     """ """
-    fpath = "/Users/johnlam/Downloads/DGX-rendering-2021_06_25/ZinD_BEV_RGB_only_2021_06_25/gt_alignment_approx/000/pair_10_floor_rgb_floor_01_partial_room_01_pano_15.jpg"
+    #fpath = "/Users/johnlam/Downloads/DGX-rendering-2021_06_25/ZinD_BEV_RGB_only_2021_06_25/gt_alignment_approx/000/pair_10_floor_rgb_floor_01_partial_room_01_pano_15.jpg"
+    fpath = '/mnt/data/johnlam/ZinD_BEV_RGB_only_2021_07_14_v3/gt_alignment_approx/1394/pair_24___opening_0_0_identity_ceiling_rgb_floor_01_partial_room_01_pano_18.jpg'
     pair_idx = pair_idx_from_fpath(fpath)
 
-    assert pair_idx == 10
+    assert pair_idx == 24
 
 
 def pano_id_from_fpath(fpath: str) -> int:
@@ -36,14 +42,20 @@ def pano_id_from_fpath(fpath: str) -> int:
 
 def test_pano_id_from_fpath() -> None:
     """ """
-    fpath = "/Users/johnlam/Downloads/DGX-rendering-2021_06_25/ZinD_BEV_RGB_only_2021_06_25/gt_alignment_approx/242/pair_58_floor_rgb_floor_01_partial_room_13_pano_25.jpg"
+    #fpath = "/Users/johnlam/Downloads/DGX-rendering-2021_06_25/ZinD_BEV_RGB_only_2021_06_25/gt_alignment_approx/242/pair_58_floor_rgb_floor_01_partial_room_13_pano_25.jpg"
+    fpath = '/mnt/data/johnlam/ZinD_BEV_RGB_only_2021_07_14_v3/gt_alignment_approx/1394/pair_24___opening_0_0_identity_ceiling_rgb_floor_01_partial_room_01_pano_18.jpg'
     pano_id = pano_id_from_fpath(fpath)
-    assert pano_id == 25
+    assert pano_id == 18
 
 
 def get_4tuples_from_list(fpaths: List[str], label_idx: int) -> List[Tuple[str, str, str, str, int]]:
-    """
-    label_idx: index of ground truth class to associate with 4-tuple
+    """Given paths for a single floor of single building, extract training/test metadata from the filepaths.
+
+    Note: pair_idx is unique for a (building, floor) but not for a building.
+
+    Args:
+        fpaths:
+        label_idx: index of ground truth class to associate with 4-tuple
     """
 
     # put each file path into a dictionary, to group them
@@ -118,7 +130,10 @@ def make_dataset(split: str, args) -> List[Tuple[str, str, str, str, int]]:
     available_building_ids = get_available_building_ids(dataset_root=f"{args.data_root}/gt_alignment_approx")
 
     # split into train and val now --> keep 85% of building_id's in train
-    split_idx = int(len(available_building_ids) * TRAIN_SPLIT_FRACTION)
+    #split_idx = int(len(available_building_ids) * TRAIN_SPLIT_FRACTION)
+
+    # TODO: remove hard-coding
+    split_idx = 0
 
     if split == "train":
         split_building_ids = available_building_ids[:split_idx]
@@ -136,7 +151,7 @@ def make_dataset(split: str, args) -> List[Tuple[str, str, str, str, int]]:
 
             for floor_id in ["floor_00", "floor_01", "floor_02", "floor_03", "floor_04"]:
 
-                fpaths = glob.glob(f"{args.data_root}/{label_name}/{building_id}/pair*_*_rgb_{floor_id}_*.jpg")
+                fpaths = glob.glob(f"{args.data_root}/{label_name}/{building_id}/pair_*___*_rgb_{floor_id}_*.jpg")
                 # here, pair_id will be unique
 
                 tuples = get_4tuples_from_list(fpaths, label_idx)
@@ -174,3 +189,24 @@ class ZindData(Dataset):
         x1, x2, x3, x4 = self.transform(x1, x2, x3, x4)
 
         return x1, x2, x3, x4, is_match, x1_fpath, x2_fpath, x3_fpath, x4_fpath
+
+
+def test_ZindData_constructor() -> None:
+    """ """
+    transform = None
+
+    from types import SimpleNamespace
+    args = SimpleNamespace(**{
+        "data_root": "/mnt/data/johnlam/ZinD_BEV_RGB_only_2021_07_14_v3"
+    })
+    dataset = ZindData(split="val", transform=transform, args=args)
+    assert len(dataset.data_list) > 0
+
+def test_ZindData_getitem() -> None:
+    """ """
+    pass
+
+if __name__ == '__main__':
+    """ """
+    test_ZindData_constructor()
+
