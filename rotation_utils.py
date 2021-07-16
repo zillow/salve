3da1f5505a3bc86238c080b83ed842c76cfb2e98
John Lambert <johnlam@zillowgroup.com>
2021-07-16 10:52:32 -0600

    move wrap_angle_deg to rotation_utils

--- rotation_utils.py
+++ rotation_utils.py
@@ -21,7 +21,7 @@ def rotmat2d(theta_deg: float) -> np.ndarray:
     return R
 
 
-def rotmat2theta_deg(R) -> float:
+def rotmat2theta_deg(R: np.ndarray) -> float:
     """Recover the rotation angle `theta` (in degrees) from the 2d rotation matrix.
     Note: the first column of the rotation matrix R provides sine and cosine of theta,
         since R is encoded as [c,-s]
@@ -31,3 +31,43 @@ def rotmat2theta_deg(R) -> float:
     c, s = R[0, 0], R[1, 0]
     theta_rad = np.arctan2(s, c)
     return float(np.rad2deg(theta_rad))
+
+
+def wrap_angle_deg(angle1: float, angle2: float):
+    """
+    https://stackoverflow.com/questions/28036652/finding-the-shortest-distance-between-two-angles/28037434
+    """
+    # mod n will wrap x to [0,n)
+    diff = (angle2 - angle1 + 180) % 360 - 180
+    if diff < -180:
+        return np.absolute(diff + 360)
+    else:
+        return np.absolute(diff)
+
+
+def test_wrap_angle_deg() -> None:
+    """ """
+    angle1 = 180
+    angle2 = -180
+    orientation_err = wrap_angle_deg(angle1, angle2)
+    assert orientation_err == 0
+
+    angle1 = -180
+    angle2 = 180
+    orientation_err = wrap_angle_deg(angle1, angle2)
+    assert orientation_err == 0
+
+    angle1 = -45
+    angle2 = -47
+    orientation_err = wrap_angle_deg(angle1, angle2)
+    assert orientation_err == 2
+
+    angle1 = 1
+    angle2 = -1
+    orientation_err = wrap_angle_deg(angle1, angle2)
+    assert orientation_err == 2
+
+    angle1 = 10
+    angle2 = 11.5
+    orientation_err = wrap_angle_deg(angle1, angle2)
+    assert orientation_err == 1.5
