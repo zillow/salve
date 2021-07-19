0a9867ec2c3b3250262be32939b10cf96164a3b7
John Lambert <johnlam@zillowgroup.com>
2021-07-19 11:59:44 -0600

    start PR curve code

--- pr_utils.py
+++ pr_utils.py
@@ -1,7 +1,15 @@
 
-from typing import Tuple
+import os
+from enum import Enum, auto
+from pathlib import Path
+from typing import Tuple, Union
 
 import numpy as np
+
+_PathLike = Union[str, "os.PathLike[str]"]
+
+class InterpType(Enum):
+    ALL = auto()
 
 
 def assign_tp_fp_fn_tn(y_true: np.ndarray, y_pred: np.ndarray) -> Tuple[int,int,int,int]:
@@ -121,3 +129,130 @@ def test_compute_precision_recall_4() -> None:
     assert np.isclose(prec, 0.5)
     assert np.isclose(rec, 0.5)
     assert np.isclose(mAcc, 2.0)
+
+
+def compute_prec_recall_curve(
+    y_true: np.ndarray, y_pred: np.ndarray, confidences: np.ndarray, n_rec_samples: int = 101, figs_fpath: _PathLike = Path("pr_plots")
+) -> None:
+    """ """
+    import pdb; pdb.set_trace()
+    recalls_interp = np.linspace(0, 1, n_rec_samples)
+
+    if not figs_fpath.is_dir():
+        figs_fpath.mkdir(parents=True, exist_ok=True)
+
+    # only 1 class of interest -- the match class
+    is_TP, is_FP, is_FN, is_TN = assign_tp_fp_fn_tn(y_true, y_pred)
+    ninst = is_TP.sum() + is_TN.sum()
+    ranks = confidences.argsort()[::-1]  # sort by last column, i.e. confidences
+    ninst = tp + fn 
+
+    tp = cls_stats[:, i].astype(bool)
+    ap_th, precisions_interp = calc_ap(tp, recalls_interp, ninst)
+
+    # from sklearn.metrics import precision_recall_curve
+    # y_true_array = np.array([m.y_true for m in measurements])
+    # y_pred_probs_array = np.array([m.prob for m in measurements])
+    # prec, rec, thresholds = precision_recall_curve(y_true_array, y_pred_probs_array)
+
+
+def calc_ap(gt_ranked: np.ndarray, recalls_interp: np.ndarray, ninst: int) -> Tuple[float, np.ndarray]:
+    """Compute precision and recall, interpolated over n fixed recall points.
+    Args:
+        gt_ranked: Ground truths, ranked by confidence.
+        recalls_interp: Interpolated recall values.
+        ninst: Number of instances of this class.
+    Returns:
+        avg_precision: Average precision.
+        precisions_interp: Interpolated precision values.
+    """
+    tp = gt_ranked
+
+    cumulative_tp = np.cumsum(tp, dtype=np.int)
+    cumulative_fp = np.cumsum(~tp, dtype=np.int)
+    cumulative_fn = ninst - cumulative_tp
+
+    precisions = cumulative_tp / (cumulative_tp + cumulative_fp + np.finfo(float).eps)
+    recalls = cumulative_tp / (cumulative_tp + cumulative_fn)
+    precisions = interp(precisions)
+    precisions_interp = np.interp(recalls_interp, recalls, precisions, right=0)
+    avg_precision = precisions_interp.mean()
+    return avg_precision, precisions_interp
+
+
+def interp(prec: np.ndarray, method: InterpType = InterpType.ALL) -> np.ndarray:
+    """Interpolate the precision over all recall levels. See equation 2 in
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.167.6629&rep=rep1&type=pdf
+    for more information.
+    Args:
+        prec: Precision at all recall levels (N,).
+        method: Accumulation method.
+    Returns:
+        prec_interp: Interpolated precision at all recall levels (N,).
+    """
+    if method == InterpType.ALL:
+        prec_interp = np.maximum.accumulate(prec[::-1])[::-1]
+    else:
+        raise NotImplementedError("This interpolation method is not implemented!")
+    return prec_interp
+
+
+
+def rank(dts: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Rank the detections in descending order according to score (detector confidence).
+    Args:
+        dts: Array of `ObjectLabelRecord` objects. (N,).
+    Returns:
+        ranked_dts: Array of `ObjectLabelRecord` objects ranked by score (N,) where N <= MAX_NUM_BOXES.
+        ranked_scores: Array of floats sorted in descending order (N,) where N <= MAX_NUM_BOXES.
+    """
+    scores = np.array([dt.score for dt in dts.tolist()])
+    ranks = scores.argsort()[::-1]
+
+    ranked_dts = dts[ranks]
+    ranked_scores = scores[ranks]
+
+    # Ensure the number of boxes considered per class is at most `MAX_NUM_BOXES`.
+    return ranked_dts[:MAX_NUM_BOXES], ranked_scores[:MAX_NUM_BOXES]
+
+
+def plot(rec_interp: np.ndarray, prec_interp: np.ndarray, cls_name: str, figs_fpath: Path) -> Path:
+    """Plot and save the precision recall curve.
+    Args:
+        rec_interp: Interpolated recall data of shape (N,).
+        prec_interp: Interpolated precision data of shape (N,).
+        cls_name: Class name.
+        figs_fpath: Path to the folder which will contain the output figures.
+    Returns:
+        dst_fpath: Plot file path.
+    """
+    plt.plot(rec_interp, prec_interp)
+    plt.title("PR Curve")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+
+    dst_fpath = Path(f"{figs_fpath}/{cls_name}.png")
+    plt.savefig(dst_fpath)
+    plt.close()
+    return dst_fpath
+
+
+
+def test_compute_prec_recall_curve() -> None:
+    """ """
+    y_true = np.array([1, 1, 0])
+    y_pred = np.array([1, 1, 1])
+    confidences = np.array([1.0, 1.0, 0.5])
+
+    import pdb; pdb.set_trace()
+    ap = compute_prec_recall_curve(y_true, y_pred, confidences, n_rec_samples = 4, figs_fpath = Path("pr_plots"))
+    assert ap == 0.67
+
+
+
+
+if __name__ == '__main__':
+    test_compute_prec_recall_curve()
+
+
+
