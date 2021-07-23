9b6767aa38aa31c92a027f04c65e29044d8af366
John Lambert <johnlam@zillowgroup.com>
2021-07-23 16:37:19 -0600

    add functoion to measure the speed of photometric shift

--- afp/utils/transform.py
+++ afp/utils/transform.py
@@ -450,8 +450,37 @@ def test_PhotometricShift_unmodified() -> None:
         assert np.allclose(image3, image3_)
         assert np.allclose(image4, image4_)
 
+def test_PhotometricShift_speed():
+    """ """
+
+    img_dir = "/Users/johnlam/Downloads/DGX-rendering-2021_07_14/ZinD_BEV_RGB_only_2021_07_14_v3/gt_alignment_approx/1583"
+
+    import imageio
+    import time
+
+    transform = PhotometricShift(jitter_types=[])
+
+    image1 = imageio.imread(f"{img_dir}/pair_28___opening_1_0_identity_floor_rgb_floor_01_partial_room_06_pano_13.jpg")
+    image2 = imageio.imread(f"{img_dir}/pair_28___opening_1_0_identity_ceiling_rgb_floor_01_partial_room_02_pano_11.jpg")
+    image3 = imageio.imread(f"{img_dir}/pair_28___opening_1_0_identity_ceiling_rgb_floor_01_partial_room_06_pano_13.jpg")
+    image4 = imageio.imread(f"{img_dir}/pair_28___opening_1_0_identity_floor_rgb_floor_01_partial_room_02_pano_11.jpg")
+
+    start = time.time()
+
+    for _ in range(10000):
+        image1_, image2_, image3_, image4_ = transform(image1, image2, image3, image4)
+
+    end = time.time()
+    duration = end - start
+    print(f"Took {duration} sec.")
+
+
+
+
 if __name__ == '__main__':
     #test_PhotometricShift_unmodified()
-    test_PhotometricShift()
+    #test_PhotometricShift()
+
+    test_PhotometricShift_speed()
 
 
