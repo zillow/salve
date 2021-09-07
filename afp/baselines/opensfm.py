adc062b798bbbc0f315b4eb0ff251dc4217ebab5
John Lambert <johnlam@zillowgroup.com>
2021-09-07 12:14:09 -0400

    reformat with python black

--- afp/baselines/opensfm.py
+++ afp/baselines/opensfm.py
@@ -1,4 +1,3 @@
-
 """
 
 https://github.com/mapillary/OpenSfM/blob/master/opensfm/io.py#L214
@@ -35,10 +34,10 @@ def panoid_from_key(key: str) -> int:
 
     Given 'floor_01_partial_room_01_pano_11.jpg', return 11
     """
-    return int(Path(key).stem.split('_')[-1])
+    return int(Path(key).stem.split("_")[-1])
 
 
-def assign_shot_attributes(obj: Dict[str, Any], shot) -> None: # pymap.Shot
+def assign_shot_attributes(obj: Dict[str, Any], shot) -> None:  # pymap.Shot
     shot.metadata = json_to_pymap_metadata(obj)
     if "scale" in obj:
         shot.scale = obj["scale"]
@@ -51,7 +50,7 @@ def assign_shot_attributes(obj: Dict[str, Any], shot) -> None: # pymap.Shot
         shot.mesh.faces = obj["faces"]
 
 
-def json_to_pymap_metadata(obj: Dict[str, Any]): # -> pymap.ShotMeasurements:
+def json_to_pymap_metadata(obj: Dict[str, Any]):  # -> pymap.ShotMeasurements:
     metadata = pymap.ShotMeasurements()
     if obj.get("orientation") is not None:
         metadata.orientation.value = obj.get("orientation")
@@ -75,28 +74,28 @@ def json_to_pymap_metadata(obj: Dict[str, Any]): # -> pymap.ShotMeasurements:
 
 
 def point_from_json(
-    key: str, obj: Dict[str, Any] # types.Reconstruction
-) -> Tuple[Tuple[float,float,float], Tuple[int,int,int]]: # -> pymap.Landmark:
+    key: str, obj: Dict[str, Any]  # types.Reconstruction
+) -> Tuple[Tuple[float, float, float], Tuple[int, int, int]]:  # -> pymap.Landmark:
     """
     Read a point from a json object
     """
-    #point = reconstruction.create_point(key, obj["coordinates"])
+    # point = reconstruction.create_point(key, obj["coordinates"])
     point = obj["coordinates"]
     color = obj["color"]
     return point, color
 
 
 def shot_in_reconstruction_from_json(
-    #reconstruction,#: types.Reconstruction,
+    # reconstruction,#: types.Reconstruction,
     key: str,
     obj: Dict[str, Any],
     is_pano_shot: bool = False,
-): # -> pymap.Shot:
+):  # -> pymap.Shot:
     """
     Read shot from a json object and append it to a reconstruction
     """
     pose = pose_from_json(obj)
-    #import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
 
     # if is_pano_shot:
     #     shot = reconstruction.create_pano_shot(key, obj["camera"], pose)
@@ -105,10 +104,10 @@ def shot_in_reconstruction_from_json(
     #     # https://github.com/mapillary/OpenSfM/blob/master/opensfm/types.py#L162
     #     shot = reconstruction.create_shot(key, obj["camera"], pose)
     # assign_shot_attributes(obj, shot)
-    return pose #shot
+    return pose  # shot
 
 
-def pose_from_json(obj: Dict[str, Any]) -> Pose3: # -> pygeometry.Pose:
+def pose_from_json(obj: Dict[str, Any]) -> Pose3:  # -> pygeometry.Pose:
     """
     The OpenSfM Pose class contains a rotation field, representing the local coordinate system as an axis-angle vector.
 
@@ -135,7 +134,7 @@ def pose_from_json(obj: Dict[str, Any]) -> Pose3: # -> pygeometry.Pose:
     # .def_property("translation", &geometry::Pose::TranslationWorldToCamera,
     #               &geometry::Pose::SetWorldToCamTranslation)
 
-    return Pose3(R,t)
+    return Pose3(R, t)
 
 
 def VectorToRotationMatrix(r: np.ndarray) -> Rot3:
@@ -146,22 +145,22 @@ def VectorToRotationMatrix(r: np.ndarray) -> Rot3:
     Returns:
         array of shape (3,3) representing 3d rotation matrix.
     """
-    n = np.linalg.norm(r) # get encoded angle (in radians)
+    n = np.linalg.norm(r)  # get encoded angle (in radians)
 
-    r = r.reshape(3,1)
+    r = r.reshape(3, 1)
 
     # AxisAngle accepts (unitAxis, angle) vs. Eigen::AngleAxisd which accepts (angle, unitAxis)
-    if (n == 0):  # avoid division by 0
+    if n == 0:  # avoid division by 0
         R = gtsam.Rot3.AxisAngle(r, 0)
-        #return Eigen::AngleAxisd(0, r).toRotationMatrix()
+        # return Eigen::AngleAxisd(0, r).toRotationMatrix()
     else:
-        #R = gtsam.Rot3.AxisAngle(unitAxis=r/n, angle=n)
-        R = gtsam.Rot3.AxisAngle(r/n, n)
-        #return Eigen::AngleAxisd(n, r / n).toRotationMatrix()
+        # R = gtsam.Rot3.AxisAngle(unitAxis=r/n, angle=n)
+        R = gtsam.Rot3.AxisAngle(r / n, n)
+        # return Eigen::AngleAxisd(n, r / n).toRotationMatrix()
     return R
 
 
-def bias_from_json(obj: Dict[str, Any]):# -> pygeometry.Similarity:
+def bias_from_json(obj: Dict[str, Any]):  # -> pygeometry.Similarity:
     """
     Args:
 
@@ -171,8 +170,7 @@ def bias_from_json(obj: Dict[str, Any]):# -> pygeometry.Similarity:
     return Similarity3(R=obj["rotation"], t=obj["translation"], s=obj["scale"])
 
 
-
-def camera_from_json(key: str, obj: Dict[str, Any]): # -> pygeometry.Camera:
+def camera_from_json(key: str, obj: Dict[str, Any]):  # -> pygeometry.Camera:
     """
     Read camera from a json object
     """
@@ -218,10 +216,10 @@ def camera_from_json(key: str, obj: Dict[str, Any]): # -> pygeometry.Camera:
     #         obj["focal"],
     #         obj.get("k1", 0.0),
     #         obj.get("k2", 0.0),
-        # )
+    # )
     if pt == "spherical" or pt == "equirectangular":
         # see https://github.com/mapillary/OpenSfM/blob/master/opensfm/src/geometry/python/pybind.cc#L169
-        #camera = pygeometry.Camera.create_spherical()
+        # camera = pygeometry.Camera.create_spherical()
         # https://github.com/mapillary/OpenSfM/blob/master/opensfm/src/geometry/src/camera.cc#L107
         camera = SimpleNamespace(**{"projection_type": "SPHERICAL", "id": None, "width": None, "height": None})
 
@@ -257,8 +255,6 @@ def load_opensfm_reconstruction_from_json(obj: Dict[str, Any]):
     # if "rig_cameras" in obj:
     #     for key, value in obj["rig_cameras"].items():
     #         reconstruction.add_rig_camera(rig_camera_from_json(key, value))
-
-    
 
     pose_dict = {}
     # Extract shots
@@ -296,17 +292,14 @@ def load_opensfm_reconstruction_from_json(obj: Dict[str, Any]):
     #         lla["latitude"], lla["longitude"], lla["altitude"]
     #     )
 
-    reconstruction = OpenSfmReconstruction(
-        camera,
-        pose_dict,
-        points,
-        rgb
-    )
+    reconstruction = OpenSfmReconstruction(camera, pose_dict, points, rgb)
     logger.info("Reconstruction found with %d cameras and %d points", len(pose_dict), points.shape[0])
     return reconstruction
 
 
-def load_opensfm_reconstructions_from_json(reconstruction_json_fpath: str) -> List[OpenSfmReconstruction]: # : # -> types.Reconstruction:
+def load_opensfm_reconstructions_from_json(
+    reconstruction_json_fpath: str,
+) -> List[OpenSfmReconstruction]:  # : # -> types.Reconstruction:
     """
     Read a reconstruction from a json object
 
@@ -328,18 +321,19 @@ def measure_opensfm_localization_accuracy(reconstruction_json_fpath: str):
 
     reconstructions = load_opensfm_reconstructions_from_json(reconstruction_json_fpath)
 
-    import pdb; pdb.set_trace()
+    import pdb
+
+    pdb.set_trace()
 
     for reconstruction in reconstructions:
         est_floor_pose_graph = PoseGraph2d.from_wRi_wti_lists(
             wRi_list, wti_list, gt_floor_pose_graph, building_id, floor_id
         )
-        mean_abs_rot_err, mean_abs_trans_err = est_floor_pose_graph.measure_abs_pose_error(gt_floor_pg=gt_floor_pose_graph)
-        est_floor_pose_graph.render_estimated_layout(
-            show_plot=False,
-            save_plot=True,
-            plot_save_dir=plot_save_dir,
+        mean_abs_rot_err, mean_abs_trans_err = est_floor_pose_graph.measure_abs_pose_error(
             gt_floor_pg=gt_floor_pose_graph
+        )
+        est_floor_pose_graph.render_estimated_layout(
+            show_plot=False, save_plot=True, plot_save_dir=plot_save_dir, gt_floor_pg=gt_floor_pose_graph
         )
 
 
@@ -347,7 +341,5 @@ if __name__ == "__main__":
     """ """
     reconstruction_json_fpath = "/Users/johnlam/Downloads/OpenSfM/data/ZinD_1442_floor_01/reconstruction.json"
 
-    #load_opensfm_reconstructions_from_json(reconstruction_json_fpath)
+    # load_opensfm_reconstructions_from_json(reconstruction_json_fpath)
     measure_opensfm_localization_accuracy(reconstruction_json_fpath)
-
-
