9b994d0f2bbf9903e458aa24dce2fb41612026d2
John Lambert <johnlam@zillowgroup.com>
2021-10-01 16:24:21 -0400

    re-arrange functions in openmvg.py for readability

--- afp/baselines/openmvg.py
+++ afp/baselines/openmvg.py
@@ -20,6 +20,73 @@ from afp.baselines.sfm_reconstruction import SfmReconstruction
 
 OPENMVG_SFM_BIN = "/Users/johnlam/Downloads/openMVG_Build/Darwin-x86_64-RELEASE"
 
+
+def panoid_from_key(key: str) -> int:
+    """Extract panorama id from panorama image file name.
+
+    Given 'floor_01_partial_room_01_pano_11.jpg', return 11
+    """
+    return int(Path(key).stem.split("_")[-1])
+
+
+def load_openmvg_reconstructions_from_json(building_id: str, floor_id: str) -> List[SfmReconstruction]:
+    """
+
+    Args:
+        building_id
+        floor_id
+
+    Returns:
+        reconstructions
+    """
+    OPEMVG_DEMO_ROOT = "/Users/johnlam/Downloads/openmvg_demo"
+
+    json_fpath = f"{OPEMVG_DEMO_ROOT}/ZinD_{building_id}_{floor_id}__2021_09_21/reconstruction/sfm_data.json"
+    data = json_utils.read_json_file(json_fpath)
+
+    assert data["sfm_data_version"] == "0.3"
+
+    intrinsics = data["intrinsics"]
+    print("OpenMVG Estimated Instrinsics: ", intrinsics)
+    view_metadata = data["views"]
+    print("OpenMVG Estimated View Metadata: ", view_metadata)
+    extrinsics = data["extrinsics"]
+
+    key_to_fname_dict = {}
+    for view in data["views"]:
+        openmvg_key = view["key"]
+        filename = view["value"]["ptr_wrapper"]["data"]["filename"]
+        key_to_fname_dict[openmvg_key] = filename
+
+    pose_dict = {}
+
+    for ext_info in extrinsics:
+        openmvg_key = ext_info["key"]
+        R = np.array(ext_info["value"]["rotation"])
+        # See https://github.com/openMVG/openMVG/issues/671
+        # and http://openmvg.readthedocs.io/en/latest/openMVG/cameras/cameras/#pinhole-camera-model
+        t = -R @ np.array(ext_info["value"]["center"])
+
+        cTw = Pose3(Rot3(R), t)
+        wTc = cTw.inverse()
+
+        filename = key_to_fname_dict[openmvg_key]
+
+        pano_id = panoid_from_key(filename)
+        pose_dict[pano_id] = wTc
+
+    reconstruction = SfmReconstruction(
+        camera=None,  # could provide spherical properties
+        pose_dict=pose_dict,
+        points=np.zeros((0, 3)),
+        rgb=np.zeros((0, 3)).astype(np.uint8),
+    )
+
+    # OpenSfM only returns the largest connected component for incremental
+    # (See Pierre Moulon's comment here: https://github.com/openMVG/openMVG/issues/1938)
+    return [reconstruction]
+
+
 def run_openmvg_commands_single_tour(image_dirpath: str, matches_dirpath: str, reconstruction_dirpath: str) -> None:
     """
 
@@ -146,71 +213,6 @@ def run_openmvg_all_tours() -> None:
             # delete copy of all of the panos
             # shutil.rmtree(dst_dir)
 
-
-def panoid_from_key(key: str) -> int:
-    """Extract panorama id from panorama image file name.
-
-    Given 'floor_01_partial_room_01_pano_11.jpg', return 11
-    """
-    return int(Path(key).stem.split("_")[-1])
-
-
-def load_openmvg_reconstructions_from_json(building_id: str, floor_id: str) -> List[SfmReconstruction]:
-    """
-
-    Args:
-        building_id
-        floor_id
-
-    Returns:
-        reconstructions
-    """
-    OPEMVG_DEMO_ROOT = "/Users/johnlam/Downloads/openmvg_demo"
-
-    json_fpath = f"{OPEMVG_DEMO_ROOT}/ZinD_{building_id}_{floor_id}__2021_09_21/reconstruction/sfm_data.json"
-    data = json_utils.read_json_file(json_fpath)
-
-    assert data["sfm_data_version"] == "0.3"
-
-    intrinsics = data["intrinsics"]
-    print("OpenMVG Estimated Instrinsics: ", intrinsics)
-    view_metadata = data["views"]
-    print("OpenMVG Estimated View Metadata: ", view_metadata)
-    extrinsics = data["extrinsics"]
-
-    key_to_fname_dict = {}
-    for view in data["views"]:
-        openmvg_key = view["key"]
-        filename = view["value"]["ptr_wrapper"]["data"]["filename"]
-        key_to_fname_dict[openmvg_key] = filename
-
-    pose_dict = {}
-
-    for ext_info in extrinsics:
-        openmvg_key = ext_info["key"]
-        R = np.array(ext_info["value"]["rotation"])
-        # See https://github.com/openMVG/openMVG/issues/671
-        # and http://openmvg.readthedocs.io/en/latest/openMVG/cameras/cameras/#pinhole-camera-model
-        t = -R @ np.array(ext_info["value"]["center"])
-
-        cTw = Pose3(Rot3(R), t)
-        wTc = cTw.inverse()
-
-        filename = key_to_fname_dict[openmvg_key]
-
-        pano_id = panoid_from_key(filename)
-        pose_dict[pano_id] = wTc
-
-    reconstruction = SfmReconstruction(
-        camera=None,  # could provide spherical properties
-        pose_dict=pose_dict,
-        points=np.zeros((0, 3)),
-        rgb=np.zeros((0, 3)).astype(np.uint8),
-    )
-
-    # OpenSfM only returns the largest connected component for incremental
-    # (See Pierre Moulon's comment here: https://github.com/openMVG/openMVG/issues/1938)
-    return [reconstruction]
 
 
 if __name__ == "__main__":
