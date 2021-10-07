14d11089f605e6caf141884c98f51e70df864c5d
John Lambert <johnlam@zillowgroup.com>
2021-10-07 12:10:16 -0400

    Reformat with python black

--- sandbox/analyze_bev_overlap.py
+++ sandbox/analyze_bev_overlap.py
@@ -1,4 +1,3 @@
-
 """
 Analyze visibility between two panoramas, as captured by the BEV overlap of texture maps.
 """
@@ -20,26 +19,23 @@ def main() -> None:
     split_building_ids = zind_data.get_available_building_ids(dataset_root=f"{data_root}/gt_alignment_approx")
 
     args = SimpleNamespace(
-    **{
-    "data_root": "",
-    "layout_data_root":  "",
-    "modalities": ["ceiling_rgb_texture", "floor_rgb_texture"]
-    })
+        **{"data_root": "", "layout_data_root": "", "modalities": ["ceiling_rgb_texture", "floor_rgb_texture"]}
+    )
 
-    label_dict = {"gt_alignment_approx": 1} #, "incorrect_alignment": 0}  # is_match = True
+    label_dict = {"gt_alignment_approx": 1}  # , "incorrect_alignment": 0}  # is_match = True
 
     floor_ious = []
 
     plot_range = [0.0, 0.1]
-    #plot_range = [0.1, 0.2]
-    #plot_range = [0.2, 0.3]
-    #plot_range = [0.3, 0.4]
-    #plot_range = [0.4, 0.5]
-    #plot_range = [0.5, 0.6]
-    #plot_range = [0.6, 0.7]
-    #plot_range = [0.7, 0.8]
-    #plot_range = [0.8, 0.9]
-    #plot_range = [0.9, 1.0]
+    # plot_range = [0.1, 0.2]
+    # plot_range = [0.2, 0.3]
+    # plot_range = [0.3, 0.4]
+    # plot_range = [0.4, 0.5]
+    # plot_range = [0.5, 0.6]
+    # plot_range = [0.6, 0.7]
+    # plot_range = [0.7, 0.8]
+    # plot_range = [0.8, 0.9]
+    # plot_range = [0.9, 1.0]
 
     for label_name, label_idx in label_dict.items():
         for b, building_id in enumerate(split_building_ids):
@@ -68,13 +64,11 @@ def main() -> None:
                     #     c2 = imageio.imread(fp2c)
                     #     show_quadruplet(c1, c2, f1, f2, title=f"IoU: {floor_iou:.2f}")
 
-
         plt.hist(floor_ious, bins=20)
         plt.xlabel("BEV Floor-Floor IoU")
         plt.ylabel("Counts")
         plt.show()
         plt.close("all")
-
 
 
 def texture_map_iou(f1: np.ndarray, f2: np.ndarray) -> float:
@@ -86,6 +80,7 @@ def texture_map_iou(f1: np.ndarray, f2: np.ndarray) -> float:
     iou = binary_mask_iou(f1_occ_mask, f2_occ_mask)
     return iou
 
+
 def test_texture_map_iou() -> None:
     """
     f1 [1,0]
@@ -94,16 +89,17 @@ def test_texture_map_iou() -> None:
     f2 [1,0]
        [1,0]
     """
-    f1 = np.zeros((2,2,3)).astype(np.uint8)
-    f1[0,0] = [0,100,0]
-    f1[1,1] = [100,0,0]
+    f1 = np.zeros((2, 2, 3)).astype(np.uint8)
+    f1[0, 0] = [0, 100, 0]
+    f1[1, 1] = [100, 0, 0]
 
-    f2 = np.zeros((2,2,3)).astype(np.uint8)
-    f2[0,0] = [0, 0, 100]
-    f2[1,0] = [0, 100, 0]
+    f2 = np.zeros((2, 2, 3)).astype(np.uint8)
+    f2[0, 0] = [0, 0, 100]
+    f2[1, 0] = [0, 100, 0]
 
     iou = texture_map_iou(f1, f2)
-    assert np.isclose(iou, 1/3)
+    assert np.isclose(iou, 1 / 3)
+
 
 def binary_mask_iou(mask1: np.ndarray, mask2: np.ndarray) -> float:
     """ """
@@ -111,6 +107,7 @@ def binary_mask_iou(mask1: np.ndarray, mask2: np.ndarray) -> float:
     inter = np.logical_and(mask1, mask2)
     union = np.logical_or(mask1, mask2)
     return inter.sum() / (union.sum() + eps)
+
 
 def test_binary_mask_iou() -> None:
     """ """
@@ -130,22 +127,24 @@ def test_binary_mask_iou() -> None:
         ])
     # fmt: on
     iou = binary_mask_iou(mask1, mask2)
-    assert np.isclose(iou, 1/8)
+    assert np.isclose(iou, 1 / 8)
 
 
-def show_quadruplet(im1: np.ndarray, im2: np.ndarray, im3: np.ndarray, im4: np.ndarray, title: Optional[str] = None) -> None:
+def show_quadruplet(
+    im1: np.ndarray, im2: np.ndarray, im3: np.ndarray, im4: np.ndarray, title: Optional[str] = None
+) -> None:
     """ """
-    plt.figure(figsize=(20,10))
-    plt.subplot(2,2,1)
+    plt.figure(figsize=(20, 10))
+    plt.subplot(2, 2, 1)
     plt.imshow(im1)
 
-    plt.subplot(2,2,2)
+    plt.subplot(2, 2, 2)
     plt.imshow(im2)
 
-    plt.subplot(2,2,3)
+    plt.subplot(2, 2, 3)
     plt.imshow(im3)
 
-    plt.subplot(2,2,4)
+    plt.subplot(2, 2, 4)
     plt.imshow(im4)
 
     if title is not None:
