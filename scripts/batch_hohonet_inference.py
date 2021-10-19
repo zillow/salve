7188f5790df4a07c03a3ddc264dce820142b4e58
John Lambert <johnlam@zillowgroup.com>
2021-10-18 22:22:00 -0400

    step through batch inference script

--- scripts/batch_hohonet_inference.py
+++ scripts/batch_hohonet_inference.py
@@ -13,16 +13,13 @@ from pathlib import Path
 from typing import SimpleNamespace
 
 import afp.utils.hohonet_inference as hohonet_inference_utils
+import imageio
 import numpy as np
 import torch
 from afp.utils.hohonet_inference import HOHONET_CONFIG_FPATH, HOHONET_CKPT_FPATH
 from tqdm import tqdm
-from imageio import imread, imwrite
 
 from lib.config import config, update_config
-
-
-
 
 
 def infer_depth_over_image_list(args: SimpleNamespace, image_fpaths: List[str]):
@@ -30,10 +27,13 @@ def infer_depth_over_image_list(args: SimpleNamespace, image_fpaths: List[str]):
     Note: Depth map only created if nonexistent.
 
     We load the model into memory only once, instead of doing so for every single individual input image.
+
+    Args:
+        args: must contain variable `building_depth_save_dir`
     """
 
     update_config(config, args)
-    device = 'cuda' if config.cuda else 'cpu'
+    device = 'cuda' # if config.cuda else 'cpu'
 
     # Init model
     model_file = importlib.import_module(config.model.file)
@@ -46,11 +46,11 @@ def infer_depth_over_image_list(args: SimpleNamespace, image_fpaths: List[str]):
     with torch.no_grad():
         for path in tqdm(image_fpaths):
 
-            fname_stem = Path(img_fpath).stem
+            fname_stem = Path(path).stem
             if Path(f"{args.building_depth_save_dir}/{fname_stem}.depth.png").exists():
                 return
 
-            rgb = imread(path)
+            rgb = imageio.imread(path)
             x = torch.from_numpy(rgb).permute(2,0,1)[None].float() / 255.
             if x.shape[2:] != config.dataset.common_kwargs.hw:
                 x = torch.nn.functional.interpolate(x, config.dataset.common_kwargs.hw, mode='area')
@@ -60,7 +60,7 @@ def infer_depth_over_image_list(args: SimpleNamespace, image_fpaths: List[str]):
                 pred_depth = pred_depth.pop('depth')
 
             fname = os.path.splitext(os.path.split(path)[1])[0]
-            imwrite(
+            imageio.imwrite(
                 os.path.join(args.out, f'{fname}.depth.png'),
                 pred_depth.mul(1000).squeeze().cpu().numpy().astype(np.uint16)
             )
@@ -81,13 +81,8 @@ def infer_depth_over_single_zind_tour(
 
     Args:
         depth_save_root: directory where depth maps should be saved.
-        bev_save_root: directory where bird's eye view texture maps should be saved.
-        hypotheses_save_root: directory where putative alignment hypotheses are saved.
         raw_dataset_dir: path to ZinD dataset.
         building_id: unique ID of ZinD building.
-        floor_id: unique ID of floor.
-        layout_save_root:
-        render_modalities: "rgb_texture", "layout"
     """
     building_depth_save_dir = f"{depth_save_root}/{building_id}"
     os.makedirs(building_depth_save_dir, exist_ok=True)
@@ -112,13 +107,6 @@ def infer_depth_over_all_zind_tours(
     raw_dataset_dir: str,
 ) -> None:
     """ """
-
-    # building_id = "000"
-    # floor_id = "floor_02" # "floor_01"
-
-    # building_id = "981" # "000"
-    # floor_id = "floor_01" # "floor_01"
-
     # discover possible building ids and floors
     building_ids = [Path(fpath).stem for fpath in glob.glob(f"{raw_dataset_dir}/*") if Path(fpath).is_dir()]
     building_ids.sort()
@@ -139,10 +127,12 @@ def infer_depth_over_all_zind_tours(
 if __name__ == "__main__":
     """ """
 
+    import pdb; pdb.set_trace()
     num_processes = 1
 
-    depth_save_root = "/Users/johnlam/Downloads/ZinD_Bridge_API_HoHoNet_Depth_Maps"
+    #depth_save_root = "/Users/johnlam/Downloads/ZinD_Bridge_API_HoHoNet_Depth_Maps"
     ## depth_save_root = "/mnt/data/johnlam/ZinD_Bridge_API_HoHoNet_Depth_Maps"
+    depth_save_root = "/data/johnlam/ZinD_Bridge_API_HoHoNet_Depth_Maps" # on se1-rmx-gpu-002
 
     # render_dataset(bev_save_root, raw_dataset_dir)
     infer_depth_over_all_zind_tours(
