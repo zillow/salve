c30ed60d2f4abc7e4159f100c93d4abb5b72a39f
John Lambert <johnlam@zillowgroup.com>
2021-11-03 22:19:56 -0400

    reformat with python black

--- scripts/measure_acc_vs_overlap.py
+++ scripts/measure_acc_vs_overlap.py
@@ -286,6 +286,7 @@ def test_measure_acc_vs_visual_overlap() -> None:
     expected_mean_acc_bins = np.array([ np.nan, 100.,   0.,  50., 100., 100., np.nan, 100., np.nan, np.nan], dtype=np.float32)
     expected_avg_rot_err_bins = np.array([ np.nan, 6.19468689, 1.4648056 , 1.95754623, 2.70051575, 0.7142812 , np.nan, 1.43321174, np.nan, np.nan])
     expected_avg_trans_err_bins = np.array([ np.nan, 0.29635385, 0.12153608, 0.06152817, 0.14050719, 0.0551777 , np.nan, 0.01080585, np.nan, np.nan])
+    import pdb; pdb.set_trace()
 
     assert np.allclose(mean_acc_bins, expected_mean_acc_bins)
     assert np.allclose(avg_rot_err_bins, expected_avg_rot_err_bins)
