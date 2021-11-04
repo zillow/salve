c9dec0238a5755ae125ab9847ff4a95bda7b0a3f
John Lambert <johnlam@zillowgroup.com>
2021-11-04 10:53:21 -0400

    fix string typo

--- train.py
+++ train.py
@@ -279,8 +279,8 @@ if __name__ == "__main__":
     # config_name = "2021_10_26_resnet50_ceiling_floor_rgbonly_no_photometric_augment.yaml"
 
     # ResNet-152 with just a single modality.
-    config_name = 2021_11_04_resnet152_ceilingonly_rgbonly_no_photometric_augment.yaml
-    # config_name = 2021_11_04_resnet152_flooronly_rgbonly_no_photometric_augment.yaml
+    config_name = "2021_11_04_resnet152_ceilingonly_rgbonly_no_photometric_augment.yaml"
+    # config_name = "2021_11_04_resnet152_flooronly_rgbonly_no_photometric_augment.yaml"
 
     with hydra.initialize_config_module(config_module="afp.configs"):
         # config is relative to the afp module
