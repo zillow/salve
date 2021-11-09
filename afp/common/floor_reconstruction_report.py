da3dcd9e35d8e6ec280121b309eced77e1ec518d
John Lambert <johnlam@zillowgroup.com>
2021-11-08 23:56:35 -0500

    move floorplan rendering to floor_reconstruction_report.py and make it not recursive

--- afp/common/floor_reconstruction_report.py
+++ afp/common/floor_reconstruction_report.py
@@ -2,9 +2,11 @@
 Stores information about a floorplan reconstruction.
 """
 
+import os
 from dataclasses import dataclass
 from typing import List, Optional
 
+import matplotlib.pyplot as plt
 import numpy as np
 from argoverse.utils.sim2 import Sim2
 
@@ -43,9 +45,12 @@ class FloorReconstructionReport:
         )
         print(f"\tAvg translation error: {mean_abs_trans_err:.2f}")
 
-        aligned_est_floor_pose_graph.render_estimated_layout(
-        #est_floor_pose_graph.render_estimated_layout(
-            show_plot=False, save_plot=True, plot_save_dir=plot_save_dir, gt_floor_pg=gt_floor_pose_graph
+        render_floorplans_side_by_side(
+            est_floor_pose_graph=aligned_est_floor_pose_graph,
+            show_plot=False,
+            save_plot=True,
+            plot_save_dir=plot_save_dir,
+            gt_floor_pg=gt_floor_pose_graph
         )
 
         print()
@@ -56,3 +61,64 @@ class FloorReconstructionReport:
             avg_abs_trans_err=mean_abs_trans_err,
             percent_panos_localized=percent_panos_localized,
         )
+
+
+def render_floorplans_side_by_side(
+    est_floor_pose_graph,
+    show_plot: bool = True,
+    save_plot: bool = False,
+    plot_save_dir: str = "floorplan_renderings",
+    gt_floor_pg: Optional[PoseGraph2d] = None,
+    plot_save_fpath: Optional[str] = None,
+) -> None:
+    """
+    Either render (show plot) or save plot to disk.
+
+    Args:
+        show_plot: boolean indicating whether to show via GUI a rendering of the plot.
+        save_plot: boolean indicating whether to save a rendering of the plot.
+        plot_save_dir: if only a general saving directory is provided for saving
+        gt_floor_pg: ground truth pose graph
+        plot_save_fpath: if a specifically desired file path is provided.
+    """
+    #import pdb; pdb.set_trace()
+    building_id = est_floor_pose_graph.building_id
+    floor_id = est_floor_pose_graph.floor_id
+
+    if gt_floor_pg is not None:
+        plt.suptitle("left: GT floorplan. Right: estimated floorplan.")
+        ax1 = plt.subplot(1, 2, 1)
+        render_floorplan(gt_floor_pg)
+        #plt.axis("equal")
+        ax1.set_aspect('equal')
+    
+    ax2 = plt.subplot(1, 2, 2, sharex=ax1, sharey=ax1)
+    ax2.set_aspect('equal')
+    render_floorplan(est_floor_pose_graph)
+    plt.title(f"Building {building_id}, {floor_id}")
+    #plt.axis("equal")
+
+    if save_plot:
+        if plot_save_dir is not None and plot_save_fpath is None:
+            os.makedirs(plot_save_dir, exist_ok=True)
+            save_fpath = f"{plot_save_dir}/{building_id}_{floor_id}.jpg"
+        elif plot_save_dir is None and plot_save_fpath is not None:
+            save_fpath = plot_save_fpath
+        plt.savefig(save_fpath, dpi=500)
+        plt.close("all")
+
+    if show_plot:
+        #plt.axis("equal")
+        plt.show()
+
+
+
+def render_floorplan(pose_graph: PoseGraph2d):
+    """Given global poses, render the floorplan by rendering each room layout in the global coordinate frame.
+
+    Args:
+    """
+    for i, pano_obj in pose_graph.nodes.items():
+        pano_obj.plot_room_layout(coord_frame="global", show_plot=False)
+
+
