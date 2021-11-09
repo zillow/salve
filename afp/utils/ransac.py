16ca3a34096f34e02c4516496a4800caa452c660
John Lambert <johnlam@zillowgroup.com>
2021-11-09 15:40:11 -0500

    dont run ransac if too few measurements

--- afp/utils/ransac.py
+++ afp/utils/ransac.py
@@ -1,4 +1,3 @@
-
 """RANSAC based Sim(3) pose alignment.
 """
 
@@ -11,18 +10,35 @@ import gtsfm.utils.geometry_comparisons as gtsfm_geometry_comparisons
 from gtsam import Rot3, Pose3, Similarity3
 
 
+DEFAULT_DELETE_FRAC = 0.33
+
+
 def ransac_align_poses_sim3_ignore_missing(
-    aTi_list_ref: List[Optional[Pose3]], bTi_list_est: List[Optional[Pose3]], num_iters = 100
+    aTi_list_ref: List[Optional[Pose3]],
+    bTi_list_est: List[Optional[Pose3]],
+    num_iters=1000,
+    delete_frac=DEFAULT_DELETE_FRAC,
 ) -> Tuple[List[Optional[Pose3]], Similarity3]:
     """
     Account for outliers in the pose graph.
-    """
-    DELETE_FRAC = 0.33
 
+    Args:
+        num_iters: number of RANSAC iterations to execture.
+        delete_frac: what percent of data to remove when fitting a single hypothesis.
+    """
     best_aligned_bTi_list_est = None
     best_aSb = None
     best_trans_error = float("inf")
     best_rot_error = float("inf")
+
+    valid_idxs = [i for i, bTi in enumerate(bTi_list_est) if bTi is not None]
+    num_to_delete = math.ceil(delete_frac * len(valid_idxs))
+    if len(valid_idxs) - num_to_delete <= 3:
+        # run without RANSAC! want at least 3 frame pairs for good alignment.
+        aligned_bTi_list_est, aSb = gtsfm_geometry_comparisons.align_poses_sim3_ignore_missing(
+            aTi_list_ref, bTi_list_est
+        )
+        return aligned_bTi_list_est, aSb
 
     # randomly delete some elements
     for _ in range(num_iters):
@@ -31,10 +47,7 @@ def ransac_align_poses_sim3_ignore_missing(
         bTi_list_est_subset = copy.deepcopy(bTi_list_est)
 
         # randomly delete 33% of the poses
-        valid_idxs = [i for i, bTi in enumerate(bTi_list_est) if bTi is not None]
-        num_to_delete = math.ceil(DELETE_FRAC * len(valid_idxs))
         delete_idxs = np.random.choice(a=valid_idxs, size=num_to_delete, replace=False)
-
         for del_idx in delete_idxs:
             bTi_list_est_subset[del_idx] = None
 
@@ -44,11 +57,11 @@ def ransac_align_poses_sim3_ignore_missing(
 
         # evaluate inliers.
         rot_error, trans_error = compute_pose_errors_3d(aTi_list_ref, aligned_bTi_list_est)
-        print("Deleted ", delete_idxs, f" -> trans_error {trans_error:.1f}, rot_error {rot_error:.1f}")
+        # print("Deleted ", delete_idxs, f" -> trans_error {trans_error:.1f}, rot_error {rot_error:.1f}")
 
         if trans_error <= best_trans_error and rot_error <= best_rot_error:
-            print(f"Found better trans error {trans_error:.2f} < {best_trans_error:.2f}")
-            print(f"Found better rot error {rot_error:.2f} < {best_rot_error:.2f}")
+            # print(f"Found better trans error {trans_error:.2f} < {best_trans_error:.2f}")
+            # print(f"Found better rot error {rot_error:.2f} < {best_rot_error:.2f}")
             best_aligned_bTi_list_est = aligned_bTi_list_est
             best_aSb = aSb
             best_trans_error = trans_error
@@ -107,17 +120,16 @@ def test_ransac_align_poses_sim3_ignore_missing() -> None:
     ]
 
     # below was previously in b's frame.
-    aTi_list_ = [ 
+    aTi_list_ = [
         None,
         Pose3(Rot3(), np.array([50.1, 0, 0])),
         Pose3(Rot3(), np.array([0, 9.9, 0])),
         Pose3(Rot3(), np.array([0, 0, 2000])),
         None,
     ]
-    
+
     aligned_bTi_list_est, aSb = ransac_align_poses_sim3_ignore_missing(aTi_list, aTi_list_)
 
     assert np.isclose(aSb.scale(), 1.0, atol=1e-2)
     assert np.allclose(aligned_bTi_list_est[1].translation(), np.array([50.0114, 0.0576299, 0]), atol=1e-3)
-    assert np.allclose(aligned_bTi_list_est[2].translation(), np.array([-0.0113879,    9.94237, 0]), atol=1e-3)
-
+    assert np.allclose(aligned_bTi_list_est[2].translation(), np.array([-0.0113879, 9.94237, 0]), atol=1e-3)
