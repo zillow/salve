82af80bd4728aa314e155a7fddbfd7ccfbb963d5
John Lambert <johnlam@zillowgroup.com>
2021-11-10 00:53:21 -0500

    move visualization code to other file

--- afp/baselines/evaluate_baseline.py
+++ afp/baselines/evaluate_baseline.py
@@ -19,19 +19,18 @@ import gtsam
 import gtsfm.utils.geometry_comparisons as geometry_comparisons
 import matplotlib.pyplot as plt
 import numpy as np
-import open3d
 import argoverse.utils.json_utils as json_utils
-from colour import Color
 from gtsam import Pose3, Rot3
-
-# import below is from gtsfm
-import visualization.open3d_vis_utils as open3d_vis_utils
 
 import afp.baselines.opensfm as opensfm_utils
 import afp.baselines.openmvg as openmvg_utils
+import afp.dataset.zind_partition as zind_partition
+import afp.visualization.utils as vis_utils
 from afp.baselines.openmvg import OPENMVG_DEMO_ROOT
+from afp.common.floor_reconstruction_report import FloorReconstructionReport
 from afp.common.posegraph2d import PoseGraph2d, get_gt_pose_graph
 from afp.common.posegraph3d import PoseGraph3d
+from afp.dataset.zind_partition import DATASET_SPLITS
 from afp.utils.logger_utils import get_logger
 
 
@@ -139,6 +138,10 @@ def measure_algorithm_localization_accuracy(
     floor_results_dicts = []
     for r, reconstruction in enumerate(reconstructions):
 
+        # just use the largest connected component for now.
+        if r > 0:
+            continue
+
         # create a 3d pose graph
         aTi_list_gt = gt_floor_pose_graph.as_3d_pose_graph()
         bTi_list_est = [reconstruction.pose_dict.get(i, None) for i in range(len(aTi_list_gt))]
@@ -157,12 +160,12 @@ def measure_algorithm_localization_accuracy(
 
         bTi_list_est = [bTi.compose(algocam_T_zillowcam) if bTi is not None else None for bTi in bTi_list_est]
 
-        # plot_3d_poses(aTi_list_gt, bTi_list_est)
+        # vis_utils.plot_3d_poses(aTi_list_gt, bTi_list_est)
 
         # align it to the 2d pose graph using Sim(3)
         aligned_bTi_list_est, _ = geometry_comparisons.align_poses_sim3_ignore_missing(aTi_list_gt, bTi_list_est)
 
-        # plot_3d_poses(aTi_list_gt, aligned_bTi_list_est)  # visualize after alignment
+        # vis_utils.plot_3d_poses(aTi_list_gt, aligned_bTi_list_est)  # visualize after alignment
 
         # project to 2d
         est_floor_pose_graph = PoseGraph3d.from_wTi_list(aligned_bTi_list_est, building_id, floor_id)
@@ -174,140 +177,33 @@ def measure_algorithm_localization_accuracy(
             reconstruction.points.shape[0],
         )
 
-        # then measure the aligned error
-        mean_abs_rot_err, mean_abs_trans_err = est_floor_pose_graph.measure_aligned_abs_pose_error(
-            gt_floor_pg=gt_floor_pose_graph
-        )
+        viz_save_dir = f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_viz_2021_11_09_largest"
+        #viz_save_dir = f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_viz_2021_11_09_largest/{building_id}_{floor_id}"
+        os.makedirs(viz_save_dir, exist_ok=True)
+        plot_save_fpath = f"{viz_save_dir}/{algorithm_name}_reconstruction_{r}.jpg"
 
-        os.makedirs(
-            f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_viz/{building_id}_{floor_id}", exist_ok=True
-        )
-        plot_save_fpath = f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_viz/{building_id}_{floor_id}/{algorithm_name}_reconstruction_{r}.jpg"
-        # render estimated layout
-        est_floor_pose_graph.render_estimated_layout(
-            show_plot=False,
-            save_plot=True,
-            plot_save_dir=None,
-            gt_floor_pg=gt_floor_pose_graph,
-            plot_save_fpath=plot_save_fpath,
+        report = FloorReconstructionReport.from_est_floor_pose_graph(
+            est_floor_pose_graph=est_floor_pose_graph,
+            gt_floor_pose_graph=gt_floor_pose_graph,
+            # plot_save_dir=None,
+            # plot_save_fpath=plot_save_fpath
+            plot_save_dir=viz_save_dir,
+            plot_save_fpath=None
         )
 
         floor_results_dict = {
             "id": f"Reconstruction {r}",
             "num_cameras": len(reconstruction.pose_dict),
             "num_points": reconstruction.points.shape[0],
-            "mean_abs_rot_err": mean_abs_rot_err,
-            "mean_abs_trans_err": mean_abs_trans_err,
+            "mean_abs_rot_err": report.avg_abs_rot_err,
+            "mean_abs_trans_err": report.avg_abs_trans_err,
         }
         floor_results_dicts.append(floor_results_dict)
 
-    os.makedirs(f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_results", exist_ok=True)
-    json_save_fpath = (
-        f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_results/{building_id}_{floor_id}.json"
-    )
+    summary_save_dir = f"/Users/johnlam/Downloads/jlambert-auto-floorplan/{algorithm_name}_zind_results_2021_11_09"
+    os.makedirs(summary_save_dir, exist_ok=True)
+    json_save_fpath = f"{summary_save_dir}/{building_id}_{floor_id}.json"
     json_utils.save_json_dict(json_save_fpath, floor_results_dicts)
-
-
-def plot_3d_poses(aTi_list_gt: List[Optional[Pose3]], bTi_list_est: List[Optional[Pose3]]) -> None:
-    """
-
-    Ground truth poses are rendered large (sphere of radius 0.5)
-    Estimated poses are rendered small (spehere of radius 0.2)
-
-    Args:
-        aTi_list_gt: list of ground truth camera poses.
-        bTi_list_est: list of estimated camera poses.
-    """
-
-    def get_colormapped_spheres(wTi_list: List[Optional[Pose3]]) -> np.ndarray:
-        """ """
-        num_valid_poses = sum([1 if wTi is not None else 0 for wTi in wTi_list])
-        colormap = get_colormap(N=num_valid_poses)
-
-        curr_color_idx = 0
-        point_cloud = []
-        rgb = []
-        for i, wTi in enumerate(wTi_list):
-            if wTi is None:
-                continue
-            point_cloud += [wTi.translation()]
-            rgb += [colormap[curr_color_idx]]
-            curr_color_idx += 1
-
-        point_cloud = np.array(point_cloud)
-        rgb = np.array(rgb)
-        return point_cloud, rgb
-
-    point_cloud_est, rgb_est = get_colormapped_spheres(bTi_list_est)
-    point_cloud_gt, rgb_gt = get_colormapped_spheres(aTi_list_gt)
-    geo1 = open3d_vis_utils.create_colored_spheres_open3d(point_cloud_est, rgb_est, sphere_radius=0.2)
-    geo2 = open3d_vis_utils.create_colored_spheres_open3d(point_cloud_gt, rgb_gt, sphere_radius=0.5)
-
-    def get_coordinate_frames(wTi_list: List[Optional[Pose3]]) -> List[open3d.geometry.LineSet]:
-        frames = []
-        for i, wTi in enumerate(wTi_list):
-            if wTi is None:
-                continue
-            frames.extend(draw_coordinate_frame(wTi))
-        return frames
-
-    frames1 = get_coordinate_frames(aTi_list_gt)
-    frames2 = get_coordinate_frames(bTi_list_est)
-
-    open3d.visualization.draw_geometries(geo1 + geo2 + frames1 + frames2)
-
-
-def get_colormap(N: int) -> np.ndarray:
-    """Obtain an RGB colormap from red to green, with N unique colors.
-
-    Args:
-        N: number of unique colors to generate.
-
-    Returns:
-        colormap: uint8 array of shape (N,3)
-    """
-    colormap = np.array([[color_obj.rgb] for color_obj in Color("red").range_to(Color("green"), N)]).squeeze()
-    colormap = (colormap * 255).astype(np.uint8)
-    return colormap
-
-
-def draw_coordinate_frame(wTc: Pose3, axis_length: float = 1.0) -> List[open3d.geometry.LineSet]:
-    """Draw 3 orthogonal axes representing a camera coordinate frame.
-
-    Note: x,y,z axes correspond to red, green, blue colors.
-
-    Args:
-        wTc: Pose of any camera in the world frame.
-        axis_length:
-
-    Returns:
-        line_sets: list of Open3D LineSet objects
-    """
-    RED = np.array([1, 0, 0])
-    GREEN = np.array([0, 1, 0])
-    BLUE = np.array([0, 0, 1])
-    colors = (RED, GREEN, BLUE)
-
-    line_sets = []
-    for axis, color in zip([0, 1, 2], colors):
-
-        lines = [[0, 1]]
-        verts_worldfr = np.zeros((2, 3))
-
-        verts_camfr = np.zeros((2, 3))
-        verts_camfr[0, axis] = axis_length
-
-        for i in range(2):
-            verts_worldfr[i] = wTc.transformFrom(verts_camfr[i])
-
-        line_set = open3d.geometry.LineSet(
-            points=open3d.utility.Vector3dVector(verts_worldfr),
-            lines=open3d.utility.Vector2iVector(lines),
-        )
-        line_set.colors = open3d.utility.Vector3dVector(color.reshape(1, 3))
-        line_sets.append(line_set)
-
-    return line_sets
 
 
 def count_panos_on_floor(raw_dataset_dir: str, building_id: str, floor_id: str) -> int:
@@ -516,8 +412,7 @@ def get_buildingid_floorid_from_json_fpath(fpath: str) -> Tuple[str, str]:
     return building_id, floor_id
 
 
-
-def eval_openmvg_errors_all_tours():
+def eval_openmvg_errors_all_tours() -> None:
     """
     Evaluate dumped sfm_data.json from every tour against ground truth.
     """
@@ -555,7 +450,93 @@ def eval_openmvg_errors_all_tours():
             )
 
 
-def main():
+def eval_opensfm_errors_all_tours() -> None:
+    """ """
+    OPENSFM_REPO_ROOT = "/Users/johnlam/Downloads/OpenSfM"
+    # reconstruction_json_fpath = "/Users/johnlam/Downloads/OpenSfM/data/ZinD_1442_floor_01/reconstruction.json"
+
+    raw_dataset_dir = "/Users/johnlam/Downloads/complete_07_10_new"
+
+    building_ids = [Path(dirpath).stem for dirpath in glob.glob(f"{raw_dataset_dir}/*")]
+    building_ids.sort()
+
+    for building_id in building_ids:
+        floor_ids = ["floor_00", "floor_01", "floor_02", "floor_03", "floor_04", "floor_05"]
+
+        for floor_id in floor_ids:
+            try:
+                src_pano_dir = f"{raw_dataset_dir}/{building_id}/panos"
+                pano_fpaths = glob.glob(f"{src_pano_dir}/{floor_id}_*.jpg")
+
+                if len(pano_fpaths) == 0:
+                    continue
+
+                FLOOR_OPENSFM_DATADIR = f"{OPENSFM_REPO_ROOT}/data/ZinD_{building_id}_{floor_id}__2021_09_13"
+                reconstruction_json_fpath = f"{FLOOR_OPENSFM_DATADIR}/reconstruction.json"
+
+                # load_opensfm_reconstructions_from_json(reconstruction_json_fpath)
+                measure_algorithm_localization_accuracy(
+                    building_id=building_id,
+                    floor_id=floor_id,
+                    raw_dataset_dir=raw_dataset_dir,
+                    algorithm_name="opensfm",
+                    reconstruction_json_fpath=reconstruction_json_fpath
+                )
+
+            except Exception as e:
+                logger.exception(f"OpenSfM failed for {building_id} {floor_id}")
+                print(f"failed on Building {building_id} {floor_id}")
+                continue
+
+
+
+def visualize_side_by_side() -> None:
+    """ """
+    import imageio
+
+    openmvg_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/openmvg_zind_viz_2021_11_09_largest"
+    opensfm_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/opensfm_zind_viz_2021_11_09_largest"
+    afp_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/2021_10_26__ResNet152__435tours_serialized_edge_classifications_test2021_11_02___2021_11_03_pgo_floorplans_with_conf_0.93"
+
+    for openmvg_fpath in glob.glob(f"{openmvg_dir}/*.jpg"):
+
+        old_building_floor_id = Path(openmvg_fpath).stem
+        k = old_building_floor_id.find("_floor")
+        old_building_id = old_building_floor_id[:k]
+        floor_id = old_building_floor_id[k+1:]
+
+        new_building_ids = zind_partition.map_old_zind_ids_to_new_ids(old_ids=[ str(int(old_building_id)) ])
+        new_building_id = new_building_ids[0]
+        if new_building_id not in DATASET_SPLITS["test"]:
+            continue
+
+        print(f"On Test ID {new_building_id}")
+        opensfm_fpath = f"{opensfm_dir}/{old_building_id}_{floor_id}.jpg"
+        afp_fpath = f"{afp_dir}/{new_building_id}_{floor_id}.jpg"
+
+        if not Path(opensfm_fpath).exists():
+            print("\tOpenSfM result missing.")
+            continue
+
+        if not Path(afp_fpath).exists():
+            print("\tAFP result missing.", afp_fpath)
+            continue
+
+        openmvg_img = imageio.imread(openmvg_fpath)
+        opensfm_img = imageio.imread(opensfm_fpath)
+        afp_img = imageio.imread(afp_fpath)
+
+        plt.figure(figsize=(20,10))
+        plt.subplot(1,3,1)
+        plt.imshow(openmvg_img)
+        plt.subplot(1,3,2)
+        plt.imshow(opensfm_img)
+        plt.subplot(1,3,3)
+        plt.imshow(afp_img)
+        plt.tight_layout()
+        plt.show()
+
+def main() -> None:
     """ """
     raw_dataset_dir = "/Users/johnlam/Downloads/complete_07_10_new"
 
@@ -564,7 +545,6 @@ def main():
     json_results_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/opensfm_zind_results"
     analyze_algorithm_results(json_results_dir, raw_dataset_dir)
     """
-
 
     # For OpenMVG
     # run_openmvg_all_tours()
@@ -575,10 +555,14 @@ def main():
 
     # reconstruction_json_fpath
 
-    eval_openmvg_errors_all_tours()
+    #eval_opensfm_errors_all_tours()
+
+    #eval_openmvg_errors_all_tours()
     # then analyze the mean statistics
-    json_results_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/openmvg_zind_results"
-    analyze_algorithm_results(json_results_dir, raw_dataset_dir)
+    # json_results_dir = "/Users/johnlam/Downloads/jlambert-auto-floorplan/openmvg_zind_results"
+    # analyze_algorithm_results(json_results_dir, raw_dataset_dir)
+
+    visualize_side_by_side()
 
 
 if __name__ == "__main__":
