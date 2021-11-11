085f966ed1ccdb01392cc9ece5f4591c68e19d3c
John Lambert <johnlam@zillowgroup.com>
2021-11-10 23:42:55 -0500

    remove repeated code for generating Sim(2) for BEV image

--- afp/utils/bev_rendering_utils.py
+++ afp/utils/bev_rendering_utils.py
@@ -26,6 +26,7 @@ import afp.utils.hohonet_pano_utils as hohonet_pano_utils
 import afp.utils.interpolation_utils as interpolation_utils
 import afp.utils.rotation_utils as rotation_utils
 import afp.utils.zorder_utils as zorder_utils
+from afp.common.bevparams import BEVParams, DEFAULT_METERS_PER_PX
 from afp.common.pano_data import WDO
 from afp.common.posegraph2d import PoseGraph2d
 from afp.utils.colormap import colormap
@@ -36,61 +37,6 @@ GREEN = [0, 255, 0]
 BLUE = [0, 0, 255]
 WDO_COLOR_DICT_CV2 = {"windows": RED, "doors": GREEN, "openings": BLUE}
 
-# the default resolution for rendering BEV images.
-
-# DEFAULT_BEV_IMG_H_PX = 2000
-# DEFAULT_BEV_IMG_W_PX = 2000
-# DEFAULT_METERS_PER_PX = 0.005
-
-# DEFAULT_BEV_IMG_H_PX = 1000
-# DEFAULT_BEV_IMG_W_PX = 1000
-# DEFAULT_METERS_PER_PX = 0.01
-
-DEFAULT_BEV_IMG_H_PX = 500
-DEFAULT_BEV_IMG_W_PX = 500
-DEFAULT_METERS_PER_PX = 0.02
-
-
-FULL_RES_METERS_PER_PX = 0.005
-
-# at 2000 x 2000 px image @ 0.005 m/px resolution, this thickness makes sense.
-FULL_RES_LINE_WIDTH_PX = 30
-
-
-def get_line_width_by_resolution(resolution: float) -> int:
-    """Compute an appropriate polyline width, in pixels, for a specific rendering resolution.
-    Note: this is not dependent upon image size -- solely dependent upon image resolution.
-    Can have a tiny image at high resolution.
-
-    Args:
-        resolution:
-    Returns:
-        line_width: line width (thickness) in pixels to use for rendering polylines with OpenCV. Must be an integer.
-    """
-    scale = resolution / FULL_RES_METERS_PER_PX
-
-    # larger scale means lower resolution, so we make the line width more narrow.
-    line_width = FULL_RES_LINE_WIDTH_PX / scale
-
-    line_width = round(line_width)
-    # must be at least 1 pixel thick.
-    return max(line_width, 1)
-
-
-def test_get_line_width_by_resolution() -> None:
-    """Ensure polyline thickness is computed properly."""
-
-    line_width = get_line_width_by_resolution(resolution=0.005)
-    assert line_width == 30
-    assert isinstance(line_width, int)
-
-    line_width = get_line_width_by_resolution(resolution=0.01)
-    assert line_width == 15
-    assert isinstance(line_width, int)
-
-    line_width = get_line_width_by_resolution(resolution=0.02)
-    assert line_width == 8
-    assert isinstance(line_width, int)
 
 
 def prune_to_2d_bbox(
@@ -114,40 +60,6 @@ def test_prune_to_2d_bbox():
     pts = prune_to_2d_bbox(pts, xmin, ymin, xmax, ymax)
     gt_pts = np.array([[1, 2], [0, 1]])
     assert np.allclose(pts, gt_pts)
-
-
-class BEVParams:
-    def __init__(
-        self,
-        img_h: int = DEFAULT_BEV_IMG_H_PX,
-        img_w: int = DEFAULT_BEV_IMG_W_PX,
-        meters_per_px: float = DEFAULT_METERS_PER_PX,
-    ) -> None:
-        """meters_per_px is resolution
-
-        1000 pixels * (0.005 m / px) = 5 meters in each direction
-        """
-        self.img_h = img_h
-        self.img_w = img_w
-        self.meters_per_px = meters_per_px
-
-        # num px in horizontal direction from center
-        h_px = img_w / 2
-
-        # num px in vertical direction from center
-        v_px = img_h / 2
-
-        # get grid boundaries in meters
-        xmin_m = -int(h_px * meters_per_px)
-        xmax_m = int(h_px * meters_per_px)
-        ymin_m = -int(v_px * meters_per_px)
-        ymax_m = int(v_px * meters_per_px)
-
-        xlims = [xmin_m, xmax_m]
-        ylims = [ymin_m, ymax_m]
-
-        self.xlims = xlims
-        self.ylims = ylims
 
 
 def rasterize_room_layout_pair(
@@ -219,10 +131,7 @@ def rasterize_single_layout(
         bev_img: array of shape (H,W,3) representing the rendered/rasterized BEV image.
     """
     HOHO_S_ZIND_SCALE_FACTOR = 1.5
-
-    grid_xmin, grid_xmax = bev_params.xlims
-    grid_ymin, grid_ymax = bev_params.ylims
-    bevimg_Sim2_world = Sim2(R=np.eye(2), t=np.array([-grid_xmin, -grid_ymin]), s=1 / bev_params.meters_per_px)
+    bevimg_Sim2_world = bev_params.bevimg_Sim2_world
 
     img_h = bev_params.img_h + 1
     img_w = bev_params.img_w + 1
@@ -362,8 +271,7 @@ def render_bev_image(bev_params: BEVParams, xyzrgb: np.ndarray, is_semantics: bo
     if num_pts == 0:
         return None
 
-    # # m/px -> px/m, then px/m * #meters = #pixels
-    bevimg_Sim2_world = Sim2(R=np.eye(2), t=np.array([-grid_xmin, -grid_ymin]), s=1 / bev_params.meters_per_px)
+    bevimg_Sim2_world = bev_params.bevimg_Sim2_world
 
     xy = xyz[:, :2]
     z = xyz[:, 2]
