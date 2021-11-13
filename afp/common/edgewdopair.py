721be861064dbbe30e2454cd706c1f17ddddfd92
John Lambert <johnlam@zillowgroup.com>
2021-11-13 11:57:13 -0500

    cast to int

--- afp/common/edgewdopair.py
+++ afp/common/edgewdopair.py
@@ -1,4 +1,6 @@
 """Contains information about a putative alignment hypothesis.
+
+TODO: perhaps deprecate this class totally in favor of EdgeClassification?
 """
 
 from typing import NamedTuple
@@ -13,6 +15,6 @@ class EdgeWDOPair(NamedTuple):
 
     @classmethod
     def from_wdo_pair_uuid(cls, i1: int, i2: int, wdo_pair_uuid: str) -> "EdgeWDOPair":
-        """ """
+        """Generate an object from a serialization ID for this W/D/O pair."""
         alignment_object, i1_wdo_idx, i2_wdo_idx = wdo_pair_uuid.split("_")
-        return cls(i1=i1, i2=i2, alignment_object=alignment_object, i1_wdo_idx=i1_wdo_idx, i2_wdo_idx=i2_wdo_idx)
+        return cls(i1=i1, i2=i2, alignment_object=alignment_object, i1_wdo_idx=int(i1_wdo_idx), i2_wdo_idx=int(i2_wdo_idx))
