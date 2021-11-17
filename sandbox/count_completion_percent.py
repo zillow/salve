3f882e27972f6372610055fdd6c07c25bc4ce771
John Lambert <johnlam@zillowgroup.com>
2021-11-17 16:34:44 -0500

    set completion percent analysis back to Megatron not se1

--- sandbox/count_completion_percent.py
+++ sandbox/count_completion_percent.py
@@ -1,5 +1,4 @@
-"""Count what percent of data from a particular split has been rendered.
-"""
+"""Count what percent of data from a particular split has been rendered on a particular machine/node."""
 
 import glob
 from pathlib import Path
@@ -17,8 +16,8 @@ def main() -> None:
     hypotheses_save_root = (
         "/home/johnlam/ZinD_bridge_api_alignment_hypotheses_madori_rmx_v1_2021_10_20_SE2_width_thresh0.65"
     )
-    #bev_save_root = "/home/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
-    bev_save_root = "/data/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
+    bev_save_root = "/home/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
+    # bev_save_root = "/data/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
 
     building_ids = [Path(d).name for d in glob.glob(f"{bev_save_root}/gt_alignment_approx/*")]
 
@@ -32,20 +31,20 @@ def main() -> None:
         for label_type in ["gt_alignment_approx", "incorrect_alignment"]:
 
             # find the floors
-            # label_hypotheses_dirpath = f"{hypotheses_save_root}/{building_id}/*/{label_type}"
-            # expected_num_label = len(glob.glob(f"{label_hypotheses_dirpath}/*"))
+            label_hypotheses_dirpath = f"{hypotheses_save_root}/{building_id}/*/{label_type}"
+            expected_num_label = len(glob.glob(f"{label_hypotheses_dirpath}/*"))
 
             label_render_dirpath = f"{bev_save_root}/{label_type}/{building_id}"
             num_rendered_label = len(glob.glob(f"{label_render_dirpath}/*")) / 4
             print(f"{label_type} {num_rendered_label}")
             
-        #     label_rendering_percent = num_rendered_label / (expected_num_label + EPS) * 100  # matches
+            label_rendering_percent = num_rendered_label / (expected_num_label + EPS) * 100  # matches
 
-        #     rendering_percent_dict[label_type] = label_rendering_percent
+            rendering_percent_dict[label_type] = label_rendering_percent
 
-        # pos_rendering_percent = rendering_percent_dict["gt_alignment_approx"]
-        # neg_rendering_percent = rendering_percent_dict["incorrect_alignment"]
-        #print(f"Building {building_id} Pos. {pos_rendering_percent:.2f}% Neg. {neg_rendering_percent:.2f}%")
+        pos_rendering_percent = rendering_percent_dict["gt_alignment_approx"]
+        neg_rendering_percent = rendering_percent_dict["incorrect_alignment"]
+        print(f"Building {building_id} Pos. {pos_rendering_percent:.2f}% Neg. {neg_rendering_percent:.2f}%")
 
 
 if __name__ == "__main__":
