deac1a8a1bc7c3ffc716f74ebe5889d4770beb71
John Lambert <johnlam@zillowgroup.com>
2021-11-23 09:32:30 -0500

    make render modalities a high-level arg to rendering script

--- render_dataset_bev.py
+++ render_dataset_bev.py
@@ -32,64 +32,6 @@ Nasty depth map estimation failure cases: (from complete_07_10 version)
 """
 
 
-def render_dataset(bev_save_root: str, raw_dataset_dir: str) -> None:
-    """Render inferred point clouds one by one.
-
-    Args:
-        bev_save_root:
-        raw_dataset_dir:
-    """
-    # building_id = "000"  # "981"  #
-    # building_id = "004"
-    building_id = "0544"
-
-    import pdb
-
-    pdb.set_trace()
-
-    img_fpaths = glob.glob(f"{raw_dataset_dir}/{building_id}/panos/*.jpg")
-
-    for img_fpath in img_fpaths:
-
-        if "_18.jpg" not in img_fpath:  # 13
-            continue
-
-        hohonet_inference_utils.infer_depth_if_nonexistent(depth_save_root, building_id, img_fpath)
-
-        is_semantics = False
-        semantic_img_fpath = f"/Users/johnlam/Downloads/MSeg_output/{Path(img_fpath).stem}_gray.jpg"
-
-        if is_semantics:
-            crop_z_range = [-float("inf"), 2]
-        else:
-            crop_z_range = [-float("inf"), float("inf")]  # -1.0]  # [0.5, float('inf')] #  # 2.0 # -1.0
-
-        args = SimpleNamespace(
-            **{
-                "img": semantic_img_fpath if is_semantics else img_fpath,
-                "depth": f"{depth_save_root}/{building_id}/{Path(img_fpath).stem}.depth.png",
-                "scale": 0.001,
-                "crop_ratio": 80 / 512,  # throw away top 80 and bottom 80 rows of pixel (too noisy of estimates)
-                "crop_z_range": crop_z_range  # 0.3 # -1.0 # -0.5 # 0.3 # 1.2
-                # "crop_z_above": 2,
-            }
-        )
-
-        bev_img = bev_rendering_utils.vis_depth_and_render(args, is_semantics=False)
-
-        bev_rendering_utils.vis_depth(args)
-
-        # save_dir = f"/Users/johnlam/Downloads/ZinD_BEV_crop_above_{args.crop_z_above}/{building_id}"
-        building_bev_save_dir = f"{bev_save_root}/{label_type}/{building_id}"
-        fname_stem = Path(img_fpath).stem
-        os.makedirs(save_dir, exist_ok=True)
-        if is_semantics:
-            img_name = f"semantics_{fname_stem}.jpg"
-        else:
-            img_name = f"{fname_stem}.jpg"
-        imageio.imwrite(f"{building_bev_save_dir}/{img_name}", bev_img)
-
-
 def panoid_from_fpath(fpath: str) -> int:
     """Derive panorama's id from its filename."""
     return int(Path(fpath).stem.split("_")[-1])
@@ -117,7 +59,7 @@ def render_building_floor_pairs(
     building_id: str,
     floor_id: str,
     layout_save_root: str,
-    render_modalities: List[str] = ["rgb_texture"],  # , "layout"]
+    render_modalities: List[str]
 ) -> None:
     """Render BEV texture maps for a single floor of a single ZinD building.
 
@@ -131,7 +73,7 @@ def render_building_floor_pairs(
         building_id: unique ID of ZinD building.
         floor_id: unique ID of floor.
         layout_save_root:
-        render_modalities: "rgb_texture", "layout"
+        render_modalities: either "rgb_texture" or "layout", or both
     """
     if "layout" in render_modalities:
         # load the layouts, either inferred or GT.
@@ -275,6 +217,7 @@ def render_pairs(
     raw_dataset_dir: str,
     hypotheses_save_root: str,
     layout_save_root: str,
+    render_modalities: List[str],
 ) -> None:
     """Render BEV texture maps for all floors of all ZinD buildings.
 
@@ -285,8 +228,8 @@ def render_pairs(
         raw_dataset_dir: path to ZinD dataset.
         hypotheses_save_root: directory where putative alignment hypotheses are saved.
         layout_save_root:
+        render_modalities:
     """
-    render_modalities = ["rgb_texture"] # ["layout"]  # 
 
     # discover possible building ids and floors
     building_ids = [Path(fpath).stem for fpath in glob.glob(f"{raw_dataset_dir}/*") if Path(fpath).is_dir()]
@@ -404,6 +347,8 @@ if __name__ == "__main__":
     layout_save_root = None
     # layout_save_root = "/home/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres_layoutimgs_inferredlayout"
 
+    render_modalities = ["rgb_texture"] # ["layout"]
+
     # render_dataset(bev_save_root, raw_dataset_dir)
     render_pairs(
         num_processes=num_processes,
@@ -412,4 +357,5 @@ if __name__ == "__main__":
         raw_dataset_dir=raw_dataset_dir,
         hypotheses_save_root=hypotheses_save_root,
         layout_save_root=layout_save_root,
+        render_modalities=render_modalities
     )
