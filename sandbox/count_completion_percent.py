a7e593127cdd47699dda166a5e6ea87746aaed53
John Lambert <johnlam@zillowgroup.com>
2021-11-24 09:36:52 -0500

    count ratio

--- sandbox/count_completion_percent.py
+++ sandbox/count_completion_percent.py
@@ -9,7 +9,7 @@ from afp.dataset.zind_partition import DATASET_SPLITS
 EPS = 1e-10
 
 
-def main() -> None:
+def check_completion() -> None:
     """ """
     desired_split = "test"
 
@@ -50,6 +50,43 @@ def main() -> None:
         print(f"Building {building_id} Pos. {pos_rendering_percent:.2f}% Neg. {neg_rendering_percent:.2f}%")
 
 
+def check_negative_positive_ratio() -> None:
+    """ """
+    ratios = []
+
+    desired_split = "test"
+
+    # hypotheses_save_root = (
+    #     "/home/johnlam/ZinD_bridge_api_alignment_hypotheses_madori_rmx_v1_2021_10_20_SE2_width_thresh0.65"
+    # )
+    # bev_save_root = "/home/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
+    # bev_save_root = "/data/johnlam/ZinD_Bridge_API_BEV_2021_10_20_lowres"
+
+    hypotheses_save_root = "/home/johnlam/ZinD_bridge_api_alignment_hypotheses_GT_WDO_2021_11_20_SE2_width_thresh0.8"
+    bev_save_root = "/home/johnlam/Renderings_ZinD_bridge_api_GT_WDO_2021_11_20_SE2_width_thresh0.8"
+
+    building_ids = [Path(d).name for d in glob.glob(f"{bev_save_root}/gt_alignment_approx/*")]
+
+    split_building_ids = DATASET_SPLITS[desired_split]
+    for building_id in sorted(building_ids):
+
+        if building_id not in split_building_ids:
+            continue
+
+        neg_hypotheses_dirpath = f"{hypotheses_save_root}/{building_id}/*/incorrect_alignment"
+        expected_num_neg = len(glob.glob(f"{neg_hypotheses_dirpath}/*"))
+
+        pos_hypotheses_dirpath = f"{hypotheses_save_root}/{building_id}/*/gt_alignment_approx"
+        expected_num_pos = len(glob.glob(f"{pos_hypotheses_dirpath}/*"))
+
+        ratio = expected_num_neg / expected_num_pos
+        print(f"Ratio on Building {building_id}: {ratio:.3f}")
+        ratios.append(ratio)
+
+    print("Average ratio: ", np.mean(ratio))
+
+
 if __name__ == "__main__":
 
-    main()
+    #check_completion()
+    check_negative_positive_ratio()
