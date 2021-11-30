0e565ebe38f3b40c51606ae5bcfbb8f6c344e306
John Lambert <johnlam@zillowgroup.com>
2021-11-30 00:57:12 -0500

    make viz_save_dir not be hard-coded, and update APIs to render world-metric with known scale

--- vis_zind_annotations.py
+++ vis_zind_annotations.py
@@ -12,15 +12,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from argoverse.utils.sim2 import Sim2
 
+import afp.common.posegraph2d as posegraph2d
 from afp.common.pano_data import PanoData, FloorData
 
 
-def render_building(building_id: str, pano_dir: str, json_annot_fpath: str) -> None:
-    """
+def render_building(building_id: str, pano_dir: str, json_annot_fpath: str, viz_save_dir: str, raw_dataset_dir: str) -> None:
+    """For a single building, render each floor.
+
     floor_map_json has 3 keys: 'scale_meters_per_coordinate', 'merger', 'redraw'
     """
-    FLOORPLANS_OUTPUT_DIR = "/Users/johnlam/Downloads/ZinD_Vis_2021_07_13"
-
     floor_map_json = json_utils.read_json_file(json_annot_fpath)
 
     if "merger" not in floor_map_json:
@@ -32,25 +32,25 @@ def render_building(building_id: str, pano_dir: str, json_annot_fpath: str) -> N
     floor_dominant_rotation = {}
     for floor_id, floor_data in merger_data.items():
 
-        # if floor_id != 'floor_02':
-        #     continue
-        # import pdb; pdb.set_trace()
-
+        # need the PoseGraph2d object to get scaling factor.
+        gt_floor_pose_graph = posegraph2d.get_gt_pose_graph(building_id, floor_id, raw_dataset_dir)
         fd = FloorData.from_json(floor_data, floor_id)
 
         wdo_objs_seen_on_floor = set()
 
         for pano_obj in fd.panos:
             wdo_objs_seen_on_floor = pano_obj.plot_room_layout(
-                coord_frame="global", wdo_objs_seen_on_floor=wdo_objs_seen_on_floor, show_plot=False
+                coord_frame="worldmetric",
+                wdo_objs_seen_on_floor=wdo_objs_seen_on_floor,
+                show_plot=False,
+                scale_meters_per_coordinate=gt_floor_pose_graph.scale_meters_per_coordinate
             )
 
         plt.legend(loc="upper right")
         plt.title(f"Building {building_id}: {floor_id}")
         plt.axis("equal")
-        building_dir = f"{FLOORPLANS_OUTPUT_DIR}/{building_id}"
-        os.makedirs(building_dir, exist_ok=True)
-        plt.savefig(f"{building_dir}/{floor_id}.jpg", dpi=500)
+        os.makedirs(viz_save_dir, exist_ok=True)
+        plt.savefig(f"{viz_save_dir}/{building_id}__{floor_id}.jpg", dpi=500)
         # plt.show()
 
         plt.close("all")
@@ -68,8 +68,8 @@ def render_building(building_id: str, pano_dir: str, json_annot_fpath: str) -> N
         #     for floor_id, zfm_poly_list in zfm_dict.items():
 
 
-def export_pano_visualizations(raw_dataset_dir: str) -> None:
-    """ """
+def export_pano_visualizations(raw_dataset_dir: str, viz_save_dir: str) -> None:
+    """For all buildings, render each floor."""
 
     building_ids = [Path(fpath).stem for fpath in glob.glob(f"{raw_dataset_dir}/*") if Path(fpath).is_dir()]
     building_ids.sort()
@@ -77,9 +77,11 @@ def export_pano_visualizations(raw_dataset_dir: str) -> None:
     for building_id in building_ids:
 
         print(f"Render floor maps for {building_id}")
-        json_annot_fpath = f"{raw_dataset_dir}/{building_id}/zfm_data.json"
+        json_annot_fpath = f"{raw_dataset_dir}/{building_id}/zind_data.json"
         pano_dir = f"{raw_dataset_dir}/{building_id}/panos"
-        render_building(building_id=building_id, pano_dir=pano_dir, json_annot_fpath=json_annot_fpath)
+        render_building(building_id=building_id, pano_dir=pano_dir, json_annot_fpath=json_annot_fpath, 
+            viz_save_dir=viz_save_dir, raw_dataset_dir=raw_dataset_dir
+            )
 
 
 if __name__ == "__main__":
@@ -91,5 +93,12 @@ if __name__ == "__main__":
         default="/Users/johnlam/Downloads/zind_bridgeapi_2021_10_05",
         help="dirpath to downloaded ZInD dataset.",
     )
+    parser.add_argument(
+        "--viz_save_dir",
+        type=str,
+        default="/Users/johnlam/Downloads/ZinD_Vis_2021_11_30",
+        help="directory where visualizations will be saved to.",
+    )
+
     args = parser.parse_args()
-    export_pano_visualizations(args.raw_dataset_dir)
+    export_pano_visualizations(args.raw_dataset_dir, args.viz_save_dir)
