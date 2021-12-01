0fa1441687e8d58b8cacd704ad2ec1334bda2029
John Lambert <johnlam@zillowgroup.com>
2021-12-01 13:04:43 -0500

    improve docstrings and use best_aSb instead of aSb in alignment

--- afp/utils/ransac.py
+++ afp/utils/ransac.py
@@ -9,21 +9,30 @@ import gtsfm.utils.geometry_comparisons as gtsfm_geometry_comparisons
 from gtsam import Rot3, Pose3, Similarity3
 
 
-DEFAULT_DELETE_FRAC = 0.33
+DEFAULT_RANSAC_ALIGNMENT_DELETE_FRAC = 0.33
 
 
 def ransac_align_poses_sim3_ignore_missing(
     aTi_list_ref: List[Optional[Pose3]],
     bTi_list_est: List[Optional[Pose3]],
     num_iters: int = 1000,
-    delete_frac: float = DEFAULT_DELETE_FRAC,
+    delete_frac: float = DEFAULT_RANSAC_ALIGNMENT_DELETE_FRAC,
+    verbose: bool = False
 ) -> Tuple[List[Optional[Pose3]], Similarity3]:
-    """
-    Account for outliers in the pose graph.
+    """Align pose graphs by estimating a Similarity(3) transformation, while accounting for outliers in the pose graph.
 
     Args:
+        aTi_list_ref: pose graph 1 (reference/target to align to).
+        bTi_list_est: pose graph 2 (to be aligned).
         num_iters: number of RANSAC iterations to execture.
         delete_frac: what percent of data to remove when fitting a single hypothesis.
+        verbose: whether to print out information about each iteration.
+
+    Returns:
+        best_aligned_bTi_list_est_full: transformed input poses previously "bTi_list" but now which
+            have the same origin and scale as reference (now living in "a" frame). Rather than
+            representing an aligned subset, this represents the entire aligned pose graph.
+        best_aSb: Similarity(3) object that aligns the two pose graphs (best among all hypotheses).
     """
     best_aligned_bTi_list_est = None
     best_aSb = None
@@ -33,7 +42,7 @@ def ransac_align_poses_sim3_ignore_missing(
     valid_idxs = [i for i, bTi in enumerate(bTi_list_est) if bTi is not None]
     num_to_delete = math.ceil(delete_frac * len(valid_idxs))
     if len(valid_idxs) - num_to_delete < 2:
-        # run without RANSAC! want at least 3 frame pairs for good alignment.
+        # run without RANSAC! want at least 2 frame pairs for good alignment.
         aligned_bTi_list_est, aSb = gtsfm_geometry_comparisons.align_poses_sim3_ignore_missing(
             aTi_list_ref, bTi_list_est
         )
@@ -49,18 +58,20 @@ def ransac_align_poses_sim3_ignore_missing(
         delete_idxs = np.random.choice(a=valid_idxs, size=num_to_delete, replace=False)
         for del_idx in delete_idxs:
             bTi_list_est_subset[del_idx] = None
-
+        
         aligned_bTi_list_est, aSb = gtsfm_geometry_comparisons.align_poses_sim3_ignore_missing(
             aTi_list_ref_subset, bTi_list_est_subset
         )
 
         # evaluate inliers.
         rot_error, trans_error, _, _ = compute_pose_errors_3d(aTi_list_ref, aligned_bTi_list_est)
-        # print("Deleted ", delete_idxs, f" -> trans_error {trans_error:.1f}, rot_error {rot_error:.1f}")
+        if verbose:
+            print("Deleted ", delete_idxs, f" -> trans_error {trans_error:.1f}, rot_error {rot_error:.1f}")
 
         if trans_error <= best_trans_error and rot_error <= best_rot_error:
-            # print(f"Found better trans error {trans_error:.2f} < {best_trans_error:.2f}")
-            # print(f"Found better rot error {rot_error:.2f} < {best_rot_error:.2f}")
+            if verbose:
+                print(f"\tFound better trans error {trans_error:.2f} < {best_trans_error:.2f}")
+                print(f"\tFound better rot error {rot_error:.2f} < {best_rot_error:.2f}")
             best_aligned_bTi_list_est = aligned_bTi_list_est
             best_aSb = aSb
             best_trans_error = trans_error
@@ -71,20 +82,21 @@ def ransac_align_poses_sim3_ignore_missing(
     for i, bTi_ in enumerate(bTi_list_est):
         if bTi_ is None:
             continue
-        best_aligned_bTi_list_est_full[i] = aSb.transformFrom(bTi_)
+        best_aligned_bTi_list_est_full[i] = best_aSb.transformFrom(bTi_)
     return best_aligned_bTi_list_est_full, best_aSb
 
 
 def compute_pose_errors_3d(
-    aTi_list_gt: List[Pose3], aligned_bTi_list_est: List[Optional[Pose3]]
+    aTi_list_gt: List[Pose3], aligned_bTi_list_est: List[Optional[Pose3]], verbose: bool = False
 ) -> Tuple[float, float, np.ndarray, np.ndarray]:
     """Compute average pose errors over all cameras (separately in rotation and translation).
 
     Note: pose graphs must already be aligned.
 
     Args:
-        aTi_list_gt: ground truth 2d pose graph.
-        aligned_bTi_list_est:
+        aTi_list_gt: ground truth 3d pose graph.
+        aligned_bTi_list_est: estimated pose graph aligned to the ground truth's "a" frame.
+        verbose: whether to print out information about pose inputs/errors.
 
     Returns:
         mean_rot_err: average rotation error per camera, measured in degrees.
@@ -92,8 +104,9 @@ def compute_pose_errors_3d(
         rot_errors: array of (K,) rotation errors, measured in degrees.
         trans_errors: array of (K,) translation errors.
     """
-    # print("aTi_list_gt: ", aTi_list_gt)
-    # print("aligned_bTi_list_est",  aligned_bTi_list_est)
+    if verbose:
+        print("aTi_list_gt: ", aTi_list_gt)
+        print("aligned_bTi_list_est",  aligned_bTi_list_est)
 
     rotation_errors = []
     translation_errors = []
@@ -109,8 +122,9 @@ def compute_pose_errors_3d(
     rotation_errors = np.array(rotation_errors)
     translation_errors = np.array(translation_errors)
 
-    # print("Rotation Errors: ", np.round(rotation_errors,1))
-    # print("Translation Errors: ", np.round(translation_errors,1))
+    if verbose:
+        print("Rotation Errors: ", np.round(rotation_errors,1))
+        print("Translation Errors: ", np.round(translation_errors,1))
     mean_rot_err = np.mean(rotation_errors)
     mean_trans_err = np.mean(translation_errors)
     return mean_rot_err, mean_trans_err, rotation_errors, translation_errors
@@ -168,7 +182,7 @@ def test_ransac_align_poses_sim3_ignore_missing() -> None:
         None,
     ]
 
-    # below was previously in b's frame.
+    # below was previously in b's frame. Has a bit of noise compared to pose graph above.
     bTi_list = [
         None,
         Pose3(Rot3(), np.array([50.1, 0, 0])),
@@ -178,7 +192,11 @@ def test_ransac_align_poses_sim3_ignore_missing() -> None:
     ]
 
     aligned_bTi_list_est, aSb = ransac_align_poses_sim3_ignore_missing(aTi_list, bTi_list)
-
     assert np.isclose(aSb.scale(), 1.0, atol=1e-2)
     assert np.allclose(aligned_bTi_list_est[1].translation(), np.array([50.0114, 0.0576299, 0]), atol=1e-3)
     assert np.allclose(aligned_bTi_list_est[2].translation(), np.array([-0.0113879, 9.94237, 0]), atol=1e-3)
+
+
+if __name__ == "__main__":
+    test_ransac_align_poses_sim3_ignore_missing()
+
