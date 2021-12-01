e86f12a5b12b6774332c2306488a61bddc1f84e9
John Lambert <johnlam@zillowgroup.com>
2021-12-01 12:38:17 -0500

    move IoU utility unit tests to own file g

--- afp/utils/iou_utils.py
+++ afp/utils/iou_utils.py
@@ -1,7 +1,5 @@
 
-"""
-Measure IoU between two texture maps or two binary masks.
-"""
+"""Utilities to measure IoU between two texture maps or two binary masks."""
 
 import numpy as np
 
@@ -23,50 +21,9 @@ def texture_map_iou(f1: np.ndarray, f2: np.ndarray) -> float:
     return iou
 
 
-def test_texture_map_iou() -> None:
-    """
-    f1 [1,0]
-       [0,1]
-
-    f2 [1,0]
-       [1,0]
-    """
-    f1 = np.zeros((2, 2, 3)).astype(np.uint8)
-    f1[0, 0] = [0, 100, 0]
-    f1[1, 1] = [100, 0, 0]
-
-    f2 = np.zeros((2, 2, 3)).astype(np.uint8)
-    f2[0, 0] = [0, 0, 100]
-    f2[1, 0] = [0, 100, 0]
-
-    iou = texture_map_iou(f1, f2)
-    assert np.isclose(iou, 1 / 3)
-
-
 def binary_mask_iou(mask1: np.ndarray, mask2: np.ndarray) -> float:
     """ """
     eps = 1e-12
     inter = np.logical_and(mask1, mask2)
     union = np.logical_or(mask1, mask2)
     return inter.sum() / (union.sum() + eps)
-
-
-def test_binary_mask_iou() -> None:
-    """ """
-    # fmt: off
-    mask1 = np.array(
-        [
-            [1, 0, 0],
-            [1, 1, 0],
-            [1, 1, 1]
-        ])
-
-    mask2 = np.array(
-        [
-            [0, 0, 1],
-            [0, 1, 1],
-            [0, 0, 0]
-        ])
-    # fmt: on
-    iou = binary_mask_iou(mask1, mask2)
-    assert np.isclose(iou, 1 / 8)
---
+++ tests/test_iou_utils.py
@@ -0,0 +1,46 @@
+
+"""Unit tests to ensure IoU is computed correctly between two masks/images."""
+
+import numpy as np
+
+import afp.utils.iou_utils as iou_utils
+
+
+def test_texture_map_iou() -> None:
+    """
+    f1 [1,0]
+       [0,1]
+
+    f2 [1,0]
+       [1,0]
+    """
+    f1 = np.zeros((2, 2, 3)).astype(np.uint8)
+    f1[0, 0] = [0, 100, 0]
+    f1[1, 1] = [100, 0, 0]
+
+    f2 = np.zeros((2, 2, 3)).astype(np.uint8)
+    f2[0, 0] = [0, 0, 100]
+    f2[1, 0] = [0, 100, 0]
+
+    iou = iou_utils.texture_map_iou(f1, f2)
+    assert np.isclose(iou, 1 / 3)
+
+def test_binary_mask_iou() -> None:
+    """ """
+    # fmt: off
+    mask1 = np.array(
+        [
+            [1, 0, 0],
+            [1, 1, 0],
+            [1, 1, 1]
+        ])
+
+    mask2 = np.array(
+        [
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ])
+    # fmt: on
+    iou = iou_utils.binary_mask_iou(mask1, mask2)
+    assert np.isclose(iou, 1 / 8)
