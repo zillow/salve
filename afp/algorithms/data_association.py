1cae99671e6dcbff743fdabd4987dbbbfca53e5f
John Lambert <johnlam@zillowgroup.com>
2021-12-02 00:08:26 -0500

    fix bug in data association (wrong association was created

--- afp/algorithms/data_association.py
+++ afp/algorithms/data_association.py
@@ -1,6 +1,9 @@
 
 
-"""
+"""Utilities for data-association for landmark-based SLAM.
+
+We create a wrapper around GTSFM's data association routines and `Keypoints' data structure.
+
 TODO: maybe only need one landmark for each WDO, since they will act as the same? locked down?
 """
 
@@ -17,10 +20,16 @@ from afp.common.pano_data import PanoData
 
 
 def get_kpt_idx(wdo_idx: int, wdo_object_type: str, pano_data: PanoData) -> int:
-    """Get index for the start vertex. Grouped by twos for (start vertex, end vertex).
+    """Get index for the start vertex of W/D/O.
+
+    Each left and right vertex (start and end) are treated as unique keypoints.
+    Note about ordering: grouped by twos for (start vertex, end vertex) for each W/D/O.
 
     Args:
         wdo_idx: for this particular object type, index within the list.
+
+    Returns:
+        kpt_idx: keypoint index for this panorama's within-room W/D/O detections.
     """
     num_openings = len(pano_data.openings)
     num_windows = len(pano_data.windows)
@@ -38,34 +47,8 @@ def get_kpt_idx(wdo_idx: int, wdo_object_type: str, pano_data: PanoData) -> int:
     return kpt_idx
 
 
-def test_get_kpt_idx() -> None:
-    """For 1 opening and 3 windows, and no doors, get keypoint index.
-
-    [s] - openings
-    [e]
-    [s] - windows
-    [e]
-    [s]
-    [e]
-    [s]
-    [e]
-    """
-    from types import SimpleNamespace
-
-    # fill with dummy objects
-    pano_data = SimpleNamespace(
-        openings=[None],
-        windows=[None,None,None],
-        doors=[]
-    )
-    wdo_idx = 1
-    wdo_object_type = "window"
-    kpt_idx = get_kpt_idx(wdo_idx, wdo_object_type, pano_data)
-    assert kpt_idx == 4
-
-
 def perform_data_association(measurements: List[EdgeClassification], pano_dict_inferred: Dict[int, PanoData]) -> List[SfmTrack2d]:
-    """Perform union-find data association.
+    """Perform data association by first creating landmarks and graph, and then running Union-Find w/ GTSFM/GTSAM.
 
     Args:
         measurements:
@@ -102,15 +85,16 @@ def perform_data_association(measurements: List[EdgeClassification], pano_dict_i
         s_1 = get_kpt_idx(wdo_idx=i, wdo_object_type=alignment_object, pano_data=pano_dict_inferred[i1])
         s_2 = get_kpt_idx(wdo_idx=j, wdo_object_type=alignment_object, pano_data=pano_dict_inferred[i2])
 
+        # by convention here, end vertex is always 1 greater than start vertex index.
         e_1 = s_1 + 1
         e_2 = s_2 + 1
         
         if m.configuration == "rotated":
-            matches_dict[(i1, i2)] += [(s_1, s_2)]
-            matches_dict[(i1, i2)] += [(e_1, e_2)]
-        else:
-            matches_dict[(i1, i2)] += [(s_1, e_2)]
+            matches_dict[(i1, i2)] += [(s_1, e_2)] # 1's start vertex, corresponds to 2's END vertex.
             matches_dict[(i1, i2)] += [(e_1, s_2)]
+        else:
+            matches_dict[(i1, i2)] += [(s_1, s_2)] # 1's start vertex corresponds to 2's START vertex.
+            matches_dict[(i1, i2)] += [(e_1, e_2)]
 
         # i1_s, i1_e = get_ith_wdo_room_endpoints_from_pano(
         #     pano_data=pano_dict_inferred[m.i1], i=i, alignment_object=alignment_object, use_rotated=
