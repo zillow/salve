70ece368b4b90e586404d805f6f7eb62cfebbfd3
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-07-05 14:02:01 -0400

    cast str to int

--- salve/dataset/salve_sfm_result_loader.py
+++ salve/dataset/salve_sfm_result_loader.py
@@ -11,6 +11,7 @@ import salve.dataset.hnet_prediction_loader as hnet_prediction_loader
 import salve.utils.zind_pano_utils as zind_pano_utils
 from salve.common.posegraph2d import PoseGraph2d
 from salve.common.pano_data import PanoData
+from salve.common.sim2 import Sim2
 
 
 @unique
