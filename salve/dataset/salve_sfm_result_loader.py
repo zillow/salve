11e2f4ec1b5a5cd6d8ea994b1dcfa9fd5dfbdfcb
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-07-05 13:40:17 -0400

    merge with pred layout

--- salve/dataset/salve_sfm_result_loader.py
+++ salve/dataset/salve_sfm_result_loader.py
@@ -46,20 +46,52 @@ def load_estimated_pose_graph(
     localization_data = io_utils.read_json_file(json_fpath)
 
     building_id = localization_data["building_id"]
+    floor_id = localization_data["floor_id"]
 
     if boundary_type in [EstimatedBoundaryType.HNET_CORNERS, EstimatedBoundaryType.HNET_DENSE]:
         hnet_floor_predictions = hnet_prediction_loader.load_hnet_predictions(
             query_building_id=building_id, raw_dataset_dir=raw_dataset_dir, predictions_data_root=predictions_data_root
         )
-        import pdb; pdb.set_trace()
+        if floor_id not in hnet_floor_predictions:
+            raise ValueError(f"Predictions missing for {floor_id} of ZInD building {building_id}.")
+        hnet_floor_predictions = hnet_floor_predictions[floor_id]
 
+    import pdb; pdb.set_trace()
     nodes = {}
     for pano_id, v in localization_data["wSi_dict"].items():
+        if boundary_type == EstimatedBoundaryType.HNET_DENSE:
+            u, v = np.arange(1024), np.round(hnet_floor_predictions[pano_id].floor_boundary)
+            # floor-wall boundary
+            room_vertices_uv = np.hstack([u.reshape(-1, 1), v.reshape(-1, 1)])
+
+        elif boundary_type == EstimatedBoundaryType.HNET_CORNERS:
+            uv = hnet_floor_predictions[pano_id].corners_in_uv
+            uv[:, 0] *= img_w
+            uv[:, 1] *= img_h
+            # ceiling (u,v) coordinates.
+            room_vertices_uv = uv[1::2]
+
+        elif boundary_type == EstimatedBoundaryType.NONE:
+            room_vertices_local_2d = np.zeros((0, 2))
+
+        # TODO: replace with actual camera height.
+        camera_height_m = 1.0
+        img_w = 1024
+        if boundary_type in [EstimatedBoundaryType.HNET_DENSE, EstimatedBoundaryType.HNET_CORNERS]:
+            layout_pts_worldmetric = zind_pano_utils.convert_points_px_to_worldmetric(
+                points_px=room_vertices_uv, image_width=img_w, camera_height_m=camera_height_m
+            )
+            # ignore y values, which are along the vertical axis
+            room_vertices_local_2d = layout_pts_worldmetric[:, np.array([0, 2])]
+
+            # TODO: add explanation for this.
+            room_vertices_local_2d[:, 0] *= -1
+
         global_Sim2_local = Sim2(v["R"], t=v["t"], s=v["s"])
         nodes[pano_id] = PanoData(
             id=pano_id,
             global_Sim2_local=global_Sim2_local,
-            room_vertices_local_2d=np.zeros((0, 2)),
+            room_vertices_local_2d=room_vertices_local_2d,
             image_path=None,
             label=None,
             doors=None,
@@ -70,7 +102,7 @@ def load_estimated_pose_graph(
 
     return PoseGraph2d(
         building_id=building_id,
-        floor_id=localization_data["floor_id"],
+        floor_id=floor_id,
         nodes=nodes,
         scale_meters_per_coordinate=localization_data["scale_meters_per_coordinate"],
     )
