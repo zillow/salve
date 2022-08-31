c93d61430049949b27d443e336906e019d8088cc
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-08-31 09:23:42 -0400

    flake8 fixes

--- salve/common/edgewdopair.py
+++ salve/common/edgewdopair.py
@@ -8,6 +8,7 @@ from typing import NamedTuple
 
 class EdgeWDOPair(NamedTuple):
     """ """
+
     i1: int
     i2: int
     alignment_object: str
@@ -18,4 +19,6 @@ class EdgeWDOPair(NamedTuple):
     def from_wdo_pair_uuid(cls, i1: int, i2: int, wdo_pair_uuid: str) -> "EdgeWDOPair":
         """Generate an object from a serialization ID for this W/D/O pair."""
         alignment_object, i1_wdo_idx, i2_wdo_idx = wdo_pair_uuid.split("_")
-        return cls(i1=i1, i2=i2, alignment_object=alignment_object, i1_wdo_idx=int(i1_wdo_idx), i2_wdo_idx=int(i2_wdo_idx))
+        return cls(
+            i1=i1, i2=i2, alignment_object=alignment_object, i1_wdo_idx=int(i1_wdo_idx), i2_wdo_idx=int(i2_wdo_idx)
+        )
