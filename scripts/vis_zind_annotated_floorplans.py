35df573b2c77938ff1ef58b4e683a3bd6129dc33
senselessdev1 <johnlambert@Hayoungs-MacBook-Pro.local>
2022-09-18 00:53:48 -0400

    clean up floorplan annotation visualization script

--- scripts/vis_zind_annotated_floorplans.py
+++ scripts/vis_zind_annotated_floorplans.py
@@ -6,41 +6,46 @@ import os
 from pathlib import Path
 from typing import Optional, Set
 
-import argoverse.utils.json_utils as json_utils
 import matplotlib.pyplot as plt
 import numpy as np
 
 import salve.common.posegraph2d as posegraph2d
+import salve.utils.logger_utils as logger_utils
+import salve.utils.matplotlib_utils as matplotlib_utils
+import salve.utils.io as io_utils
 from salve.common.pano_data import PanoData, FloorData
 from salve.common.sim2 import Sim2
 
+logger = logger_utils.get_logger()
 
 def render_building(
-    building_id: str, pano_dir: str, json_annot_fpath: str, viz_save_dir: str, raw_dataset_dir: str
+    raw_dataset_dir: str, building_id: str, viz_save_dir: str, 
 ) -> None:
-    """For a single building, render each floor.
+    """For a single building, render each floor and save plot.
 
-    floor_map_json has 3 keys: 'scale_meters_per_coordinate', 'merger', 'redraw'
+    We use the "merger" (pre-redraw) ZInD annotations, instead of "redraw".
+    See https://github.com/zillow/zind#what-are-the-important-issues-associated-with-the-dataset-that-we-need-to-be-aware-of
+    for more information.
 
     Args:
-        building_id: unique ID of ZInD building.
-        pano_dir:
-        json_annot_fpath: path to GT annotation file (zind_data.json) for this building.
-        viz_save_dir: directory where visualizations will be saved to.
         raw_dataset_dir: dirpath to downloaded ZInD dataset.
+        building_id: unique ID of ZInD building.
+        viz_save_dir: directory where visualizations will be saved to.
     """
-    floor_map_json = json_utils.read_json_file(json_annot_fpath)
+    # Get path to GT annotation file (zind_data.json) for this building.
+    json_annot_fpath = f"{raw_dataset_dir}/{building_id}/zind_data.json"
+    pano_dir = f"{raw_dataset_dir}/{building_id}/panos"
+    floor_map_json = io_utils.read_json_file(json_annot_fpath)
 
     if "merger" not in floor_map_json:
-        print(f"Building {building_id} missing `merger` data, skipping...")
-        return
+        raise ValueError(f"Building {building_id} missing `merger` data")
 
     merger_data = floor_map_json["merger"]
 
     floor_dominant_rotation = {}
     for floor_id, floor_data in merger_data.items():
 
-        # need the PoseGraph2d object to get scaling factor.
+        # Need the PoseGraph2d object to get scaling factor.
         gt_floor_pose_graph = posegraph2d.get_gt_pose_graph(building_id, floor_id, raw_dataset_dir)
         fd = FloorData.from_json(floor_data, floor_id)
 
@@ -51,27 +56,15 @@ def render_building(
                 scale_meters_per_coordinate=gt_floor_pose_graph.scale_meters_per_coordinate,
             )
         ax = None
-        matplotlib_utils.legend_without_duplicate_labels(ax)
+        # Note: many of the Matplotlib "labels" are duplicated, but can be de-duplicated here during legend creation.
+        #matplotlib_utils.legend_without_duplicate_labels(ax)
         plt.legend(loc="upper right")
         plt.title(f"Building {building_id}: {floor_id}")
         plt.axis("equal")
         os.makedirs(viz_save_dir, exist_ok=True)
         plt.savefig(f"{viz_save_dir}/{building_id}__{floor_id}.jpg", dpi=500)
         # plt.show()
-
         plt.close("all")
-
-        # dominant_rotation, _ = determine_rotation_angle(room_vertices_global)
-        # if dominant_rotation is None:
-        #     continue
-        # floor_dominant_rotation[floor_id] = dominant_rotation
-
-        # geometry_to_visualize = ["raw", "complete", "visible", "redraw"]
-        # for geometry_type in geometry_to_visualize:
-
-        #         zfm_dict = zfm._zfm[geometry_type]
-        #         floor_maps_files_list = zfm.render_top_down_zfm(zfm_dict, output_folder_floor_plan)
-        #     for floor_id, zfm_poly_list in zfm_dict.items():
 
 
 def export_floorplan_visualizations(raw_dataset_dir: str, viz_save_dir: str) -> None:
@@ -82,15 +75,11 @@ def export_floorplan_visualizations(raw_dataset_dir: str, viz_save_dir: str) -> 
 
     for building_id in building_ids:
 
-        print(f"Render floor maps for {building_id}")
-        json_annot_fpath = f"{raw_dataset_dir}/{building_id}/zind_data.json"
-        pano_dir = f"{raw_dataset_dir}/{building_id}/panos"
+        logger.info(f"Render floor maps for {building_id}")
         render_building(
-            building_id=building_id,
-            pano_dir=pano_dir,
-            json_annot_fpath=json_annot_fpath,
-            viz_save_dir=viz_save_dir,
             raw_dataset_dir=raw_dataset_dir,
+            building_id=building_id,
+            viz_save_dir=viz_save_dir,
         )
 
 
@@ -100,15 +89,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--raw_dataset_dir",
         type=str,
-        default="/Users/johnlam/Downloads/zind_bridgeapi_2021_10_05",
-        help="dirpath to downloaded ZInD dataset.",
+        default="/Users/johnlambert/Downloads/zind_bridgeapi_2021_10_05",
+        help="Path to directory containing downloaded ZInD dataset (from Bridge API).",
     )
     parser.add_argument(
         "--viz_save_dir",
         type=str,
-        default="/Users/johnlam/Downloads/ZinD_Vis_2021_11_30",
+        default="/Users/johnlambert/Downloads/ZinD_Vis_2022_02_30",
         help="directory where visualizations will be saved to.",
     )
 
     args = parser.parse_args()
-    export_floorplan_visualizations(args.raw_dataset_dir, args.viz_save_dir)
+    export_floorplan_visualizations(raw_dataset_dir=args.raw_dataset_dir, viz_save_dir=args.viz_save_dir)
