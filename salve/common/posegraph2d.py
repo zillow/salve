e2275f1361cbb50c29525a437c0a15275bc659e7
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-09-27 10:48:35 -0400

    add website pointers

--- salve/common/posegraph2d.py
+++ salve/common/posegraph2d.py
@@ -41,6 +41,12 @@ ZIND_AVERAGE_SCALE_METERS_PER_COORDINATE = 3.5083
 class PoseGraph2d(NamedTuple):
     """Pose graph for a single floor.
 
+    We use the notation `wTi = (wRi, wti)` to represent the 3d or 2d pose of the i`th camera in the world frame `w`,
+    such that p_w = wTi * p_i.
+
+    We use the notation `wSi = (wRi, wti, s)` to represent a Similarity(2) or Similarity(3) transformation,
+    such that p_w = wSi * p_i.
+
     Note: edges are not included here, since there are different types of adjacency (spatial vs. visible).
     We do not support serialization from SALVe coordinate systems back to ZInD. We only support a 1-way
     mapping from ZInD coordinates systems to SALVe.
