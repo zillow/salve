a3c0285040c8e9bc171a217a4a266a96c0ec4749
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-09-28 09:49:12 -0400

    fix broken unit test and move polyline interp unit tests to separate file

--- tests/utils/test_overlap_utils.py
+++ tests/utils/test_overlap_utils.py
@@ -8,7 +8,6 @@ import numpy as np
 from shapely.geometry import Point, Polygon
 
 import salve.utils.overlap_utils as overlap_utils
-from salve.utils.interpolate import interp_arc
 
 
 def test_determine_invalid_wall_overlap1() -> None:
@@ -81,20 +80,21 @@ def test_determine_invalid_wall_overlap_identical_shape() -> None:
         ])
 
     # fmt: on
-    wall_buffer_m = 0.2  # 20 centimeter noise buffer
 
-    import pdb; pdb.set_trace()
+    # Noise butter.
+    shrink_factor = 0.2
+
     is_valid = overlap_utils.determine_invalid_wall_overlap(
-        pano1_room_vertices=pano1_room_vertices, pano2_room_vertices=pano2_room_vertices, shrink_factor=wall_buffer_m
+        pano1_room_vertices=pano1_room_vertices, pano2_room_vertices=pano2_room_vertices, shrink_factor=shrink_factor
     )
-    # Identical shapes are not OK. Although they would mean that we have two very accurate room layout predictions within
-    # the same room, our code does not allow them BECAUSE ???
-    # TODO: failing
-    assert not is_valid
+    # Identical shapes are OK. They would mean that we have two very accurate room layout predictions within
+    # the same room. Original exterior vertices do not fall within the shrunken version.
+    assert is_valid
 
 
 def test_determine_invalid_wall_overlap3() -> None:
-    """
+    """Ensures that two stacked shapes of obviously different size (square and then its half-rectangle) are invalid.
+    
     panos (0,8) below. similar to
 
     TODO: determine why this was considered valid for Building "0003" and panos (0.7)
@@ -113,7 +113,7 @@ def test_determine_invalid_wall_overlap3() -> None:
         [[-0.08913514, -1.02572344], [-2.17362494, 1.34324966], [-0.15560001, 3.11893567], [1.92888979, 0.74996256]]
     )
     shrink_factor = 0.1
-    visualize = True
+    visualize = False
 
     freespace_is_valid = overlap_utils.determine_invalid_wall_overlap(
         pano1_room_vertices=pano1_room_vertices,
@@ -125,107 +125,7 @@ def test_determine_invalid_wall_overlap3() -> None:
         j=j,
         visualize=visualize,
     )
-
-    assert False
-
-
-def test_eliminate_duplicates_2d() -> None:
-    """Ensure two duplicated polyline waypoints are removed.
-
-    Duplicates are located at indices 4,5 and 6,7, so rows 5 and 7 should be removed.
-    """
-    polyline = np.array(
-        [
-            [3.41491678, 0.82735686],  # 0
-            [2.5812492, -2.36060637],  # 1
-            [0.2083626, -1.74008522],  # 2
-            [0.53871724, -0.47680178],  # 3
-            [0.40395381, -0.4415605],  # 4
-            [0.40395381, -0.4415605],  # 5
-            [-0.36244272, -0.24114416],  # 6
-            [-0.36244272, -0.24114416],  # 7
-            [-0.56108295, -0.18919879],  # 8
-            [-0.14397634, 1.40582611],  # 9
-            [0.06767395, 1.35047855],  # 10
-            [0.15388028, 1.68013345],  # 11
-        ]
-    )
-
-    px, py = overlap_utils.eliminate_duplicates_2d(px=polyline[:, 0], py=polyline[:, 1])
-    polyline_no_dups = np.stack([px, py], axis=-1)
-
-    expected_polyline_no_dups = np.array(
-        [
-            [3.41491678, 0.82735686],
-            [2.5812492, -2.36060637],
-            [0.2083626, -1.74008522],
-            [0.53871724, -0.47680178],
-            [0.40395381, -0.4415605],
-            [-0.36244272, -0.24114416],
-            [-0.56108295, -0.18919879],
-            [-0.14397634, 1.40582611],
-            [0.06767395, 1.35047855],
-            [0.15388028, 1.68013345],
-        ]
-    )
-    assert len(polyline_no_dups) == len(polyline) - 2
-    assert np.allclose(polyline_no_dups, expected_polyline_no_dups)
-
-
-def test_interp_arc() -> None:
-    """ """
-    polyline = np.array(
-        [
-            [ 3.41491678,  0.82735686],
-            [ 2.5812492 , -2.36060637],
-            [ 0.2083626 , -1.74008522],
-            [ 0.53871724, -0.47680178],
-            [ 0.40395381, -0.4415605 ],
-            [ 0.40395381, -0.4415605 ],
-            [-0.36244272, -0.24114416],
-            [-0.36244272, -0.24114416],
-            [-0.56108295, -0.18919879],
-            [-0.14397634,  1.40582611],
-            [ 0.06767395,  1.35047855],
-            [ 0.15388028,  1.68013345]
-        ]
-    )
-    n_waypoints = 104
-    import pdb; pdb.set_trace()
-    # Currently, only 1 duplicated index is allowed in `interpolate.py` due to legacy Argoverse reasons?
-    # TODO: failing
-    interp_polyline = interp_arc(t=n_waypoints, px=polyline[:, 0], py=polyline[:, 1])
-
-    assert isinstance(interp_polyline, np.ndarray)
-
-
-def test_interp_evenly_spaced_points() -> None:
-    """Ensures that 2d polyline interpolation at a fixed-length waypoint discretization is successful."""
-
-    # Assume vertices are given in units of meters.
-    pano2_room_vertices = np.array(
-        [
-            [3.41491678, 0.82735686],
-            [2.5812492, -2.36060637],
-            [0.2083626, -1.74008522],
-            [0.53871724, -0.47680178],
-            [0.40395381, -0.4415605],
-            [0.40395381, -0.4415605],
-            [-0.36244272, -0.24114416],
-            [-0.36244272, -0.24114416],
-            [-0.56108295, -0.18919879],
-            [-0.14397634, 1.40582611],
-            [0.06767395, 1.35047855],
-            [0.15388028, 1.68013345],
-        ]
-    )
-    pano2_room_vertices_interp = overlap_utils.interp_evenly_spaced_points(pano2_room_vertices, interval_m=0.1)
-    assert isinstance(pano2_room_vertices_interp, np.ndarray)
-    assert pano2_room_vertices_interp.shape == (104, 2)
-
-    waypoint_separations = np.linalg.norm(np.diff(pano2_room_vertices_interp, axis=0), axis=1)
-    assert np.allclose(waypoint_separations, 0.1, atol=0.03)
-    assert np.isclose(np.mean(waypoint_separations), 0.1, atol=1e-3)
+    assert not freespace_is_valid
 
 
 def test_shrink_polygon() -> None:
@@ -275,20 +175,3 @@ def test_shrink_polygon() -> None:
     # plt.show()
 
     assert isinstance(shrunk_poly1_verts, np.ndarray)
-
-
-if __name__ == "__main__":
-
-    #test_determine_invalid_wall_overlap1()
-    #test_determine_invalid_wall_overlap_identical_shape()
-
-
-    # test_shrink_polygon()
-
-    test_interp_evenly_spaced_points()
-    #test_interp_arc()
-    # test_eliminate_duplicates_2d()
-
-    # test_determine_invalid_wall_overlap1()
-    # test_determine_invalid_wall_overlap2()
-    # test_determine_invalid_wall_overlap3()
