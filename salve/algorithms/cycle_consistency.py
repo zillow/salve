22a922a06f5e539b75843bf5e4de422203104405
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-10-29 00:21:10 -0400

    move two view estimation report to own file per class under common

--- salve/algorithms/cycle_consistency.py
+++ salve/algorithms/cycle_consistency.py
@@ -15,29 +15,13 @@ import numpy as np
 
 import salve.utils.pr_utils as pr_utils
 from salve.common.sim2 import Sim2
+from salve.common.two_view_estimation_report import TwoViewEstimationReport
 from salve.utils.rotation_utils import rotmat2theta_deg
 
 logger = logging.getLogger(__name__)
 
 
 ROT_CYCLE_ERROR_THRESHOLD = 0.5  # 1.0 # 5.0 # 2.5 # 1.0 # 0.3
-
-
-@dataclass(frozen=False)
-class TwoViewEstimationReport:
-    """TODO(john): improve documentation here.
-
-    Args:
-        gt_class: ground truth category, 0 represents negative (WDO mismatch), and 1 represents positive (a WDO match)
-        R_error_deg: error in rotation, measured ...
-        U_error_deg: error in translation (Unit2), measured in ...
-        confidence: scalar-valued confidence in [0,1] range
-    """
-
-    gt_class: int
-    R_error_deg: Optional[float] = None
-    U_error_deg: Optional[float] = None
-    confidence: Optional[float] = None
 
 
 def extract_triplets(i2Ri1_dict: Dict[Tuple[int, int], np.ndarray]) -> List[Tuple[int, int, int]]:
