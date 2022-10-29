f374553c89a0245c29f193d1022472b729cf5cb0
senselessdev1 <johnlambert@hayoungs-mbp.lan>
2022-10-29 00:36:18 -0400

    improve documentation

--- salve/common/two_view_estimation_report.py
+++ salve/common/two_view_estimation_report.py
@@ -1,4 +1,4 @@
-""" """
+"""Data structure that represents rotation and translational of an edge, with respect to ground truth."""
 
 
 from typing import Optional
@@ -6,13 +6,14 @@ from dataclasses import dataclass
 
 @dataclass(frozen=False)
 class TwoViewEstimationReport:
-    """TODO(john): improve documentation here.
+    """Data structure that represents rotation and translational of an edge, with respect to ground truth.
 
     Args:
-        gt_class: ground truth category, 0 represents negative (WDO mismatch), and 1 represents positive (a WDO match)
+        gt_class: ground truth category, 0 represents negative (W/D/O mismatch), and 1 represents
+            positive (a W/D/O match).
         R_error_deg: error in rotation, measured ...
         U_error_deg: error in translation (Unit2), measured in ...
-        confidence: scalar-valued confidence in [0,1] range
+        confidence: scalar-valued confidence in [0,1] range.
     """
 
     gt_class: int
